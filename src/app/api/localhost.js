9fdcffc2f61a6503630428c4d6075a5299a6808a
panda-sas <panda.sas20@gmail.com>
2021-07-06 15:17:22 -0500

    Created Screen Dashboard page

--- src/app/api/localhost.js
+++ src/app/api/localhost.js
@@ -2,21 +2,17 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import history from "../../history";
 
-
-
 /* Uniport API Service Settings */
 var axiosLocalhostInstance = new axios.create({
   headers: {
-    'Content-Type': 'application/json;charset=UTF-8'
-}
+    "Content-Type": "application/json;charset=UTF-8",
+  },
 });
 axiosLocalhostInstance.defaults.baseURL = "/data/";
 
 /* Format response body */
 
 const responseBody = (response) => response.data;
-
-
 
 /* TYPES OF REQUESTES SUPPORTED */
 const requests = {
@@ -75,12 +71,17 @@ axiosLocalhostInstance.interceptors.response.use(undefined, (error) => {
 /* APIS */
 const Target = {
   list: () => requests.get(`/targets/targets.json`),
-  view: (id) => requests.get(`/targets/target-view.json`)
+  view: (id) => requests.get(`/targets/target-view.json`),
 };
 
+const Screen = {
+  list: () => requests.get(`/screens/screens.json`),
+  view: (id) => requests.get(`/screens/screen-view.json`),
+};
 
 const exports = {
-  Target
+  Target,
+  Screen
 };
 
 export default exports;
