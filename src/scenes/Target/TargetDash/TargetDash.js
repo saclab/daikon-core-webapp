ca1c7cfe70acb6b8bdaee4deb888db8eaba6646d
sidxz <rath.siddhant@outlook.com>
2021-07-06 15:57:02 -0500

    visualization adjust screen dash

--- src/scenes/Target/TargetDash/TargetDash.js
+++ src/scenes/Target/TargetDash/TargetDash.js
@@ -5,17 +5,16 @@ import TargetDashTable from "./TargetDashTable/TargetDashTable";
 import { RootStoreContext } from "../../../app/stores/rootStore";
 import Loading from "../../../app/layout/Loading/Loading";
 const TargetDash = () => {
+  /* MobX Store */
+  const rootStore = useContext(RootStoreContext);
+  const { fetchTargetList, displayLoading } = rootStore.targetStore;
 
-   /* MobX Store */
-   const rootStore = useContext(RootStoreContext);
-   const { fetchTargetList, displayLoading } = rootStore.targetStore;
- 
-   /* Local State Management */
- 
-   useEffect(() => {
-     console.log("TargetSearch: fetchTargetList()");
-     fetchTargetList();
-   }, [fetchTargetList]); // eslint-disable-line react-hooks/exhaustive-deps
+  /* Local State Management */
+
+  useEffect(() => {
+    console.log("TargetSearch: fetchTargetList()");
+    fetchTargetList();
+  }, [fetchTargetList]); // eslint-disable-line react-hooks/exhaustive-deps
 
   /** Loading Overlay */
   if (displayLoading) {
@@ -28,16 +27,16 @@ const TargetDash = () => {
 
       <div className="p-d-flex">
         <div className="p-mb-2">
-          <Fieldset legend="Target Map">
-            <TargetDashChart style={{ width: "1000px" }} />
-          </Fieldset>
-        </div>
-      </div>
-      <div className="p-d-flex">
-        <div className="p-mb-2">
-          <Fieldset legend="Target List">
-            <TargetDashTable />
-          </Fieldset>
+          
+            <div className="p-d-flex">
+              <div className="p-mr-2">
+                <TargetDashChart />
+              </div>
+              <div className="p-mr-2">
+                <TargetDashTable />
+              </div>
+            </div>
+         
         </div>
       </div>
     </div>
