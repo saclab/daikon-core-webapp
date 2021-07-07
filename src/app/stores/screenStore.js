8f860ae5a89c83ba3c527aebd6136305ac7339c9
sidxz <rath.siddhant@outlook.com>
2021-07-07 14:47:55 -0500

    changes to screen store

--- src/app/stores/screenStore.js
+++ src/app/stores/screenStore.js
@@ -15,48 +15,72 @@ import {
     
     historyDisplayLoading = false;
   
+    /* This is a list of targets beeing screened */
+    screenedTargetsRegistry = new Map();
+    screenedTargetsRegistryExpanded = new Map();
+    selectedScreenedTarget = null;
+
+    /* This is a list of screens for a specific target */
+    /* For caching purposes we are maintaining a single map for all screens as guid will be unique anyway */
     screenRegistry = new Map();
-    screenRegistryExpanded = new Map();
     screenHistoryRegistry = new Map();
     selectedScreen = null;
     selectedScreenHistory = null;
-  
+
+    /* This is a list of hits of a specific screen */
+    /* For caching purposes we are maintaining a single map for all hits as guid will be unique anyway */
+    hitRegistry = new Map();
+    hitHistoryRegistry = new Map();
+    selectedHit = null;
+    selectedHitHistory = null;
+
+
     constructor(rootStore) {
       this.rootStore = rootStore;
       makeObservable(this, {
         displayLoading: observable,
-        historyDisplayLoading: observable,
-  
+        // historyDisplayLoading: observable,
+
+        screenedTargets : computed,
+        fetchScreenedTargets : action,
+        screenedTargetsRegistry : observable,
+
+        selectedScreenedTarget : observable,
+        
         screens: computed,
-        fetchScreenList: action,
+        fetchScreens: action,
         screenRegistry: observable,
   
-        screen: computed,
-        fetchScreen: action,
-        selectedScreen: observable,
+        // screen: computed,
+        // fetchScreen: action,
+        // selectedScreen: observable,
   
-        fetchScreenHistory: action,
-        selectedScreenHistory: observable,
-        screenHistory: computed,
+        // fetchScreenHistory: action,
+        // selectedScreenHistory: observable,
+        // screenHistory: computed,
+
+
+        // hits: computed,
+        // fetchHits : action,
+        // hitRegistry : observable,  
   
-  
-        editScreen: action,
-        cancelEditScreen: action,
+        // editScreen: action,
+        // cancelEditScreen: action,
       });
     }
   
-    /* Fetch Screen list from API */
-    fetchScreenList = async () => {
-      console.log("screenStore: fetchScreenList() Start");
+    /* Fetch ScreenedTarget list from API */
+    fetchScreenedTargets = async () => {
+      console.log("screenStore: fetchScreenedTargets() Start");
       this.displayLoading = true;
       try {
-        var resp = await localhost.Screen.list();
+        var resp = await localhost.Screen.screenedTargets();
         runInAction(() => {
           console.log(resp);
-          resp.forEach((fetchedScreen) => {
-            this.screenRegistry.set(fetchedScreen.id, fetchedScreen);
+          resp.forEach((fetchedScreenedTarget) => {
+            this.screenedTargetsRegistry.set(fetchedScreenedTarget.id, fetchedScreenedTarget);
           });
-          console.log(this.screenRegistry);
+          console.log(this.screenedTargetsRegistry);
         });
       } catch (error) {
         console.log(error);
@@ -67,126 +91,51 @@ import {
       }
     };
   
-    get screens() {
-      console.log("screenStore: screens() Start");
-      return Array.from(this.screenRegistry.values());
+    get screenedTargets() {
+      console.log("screenStore: screenedTargets() Start");
+      return Array.from(this.screenedTargetsRegistry.values());
     }
   
-    /* Fetch specific Screen with id from API */
+    /* Fetch screens for a ScreenedTarget from API */
   
-    fetchScreen = async (id) => {
-      console.log("screenStore: fetchScreen Start");
+    fetchScreens = async (targetId) => {
+      console.log("screenStore: fetchScreens Start");
       this.displayLoading = true;
   
       // first check cache
-      let fetchedScreen = this.screenRegistryExpanded.get(id);
+      let fetchedScreens = this.screenRegistry.get(targetId);
       console.log("CACHE");
-      console.log(fetchedScreen);
-      if (fetchedScreen && fetchedScreen.hasOwnProperty("screenPromotionForm")) {
-        console.log("screenStore: fetchScreen found in cache");
-        this.selectedScreen = fetchedScreen;
+      console.log(fetchedScreens);
+      if (fetchedScreens) {
+        console.log("screenStore: fetchScreens found in cache");
+        this.selectedScreenedTarget = fetchedScreens;
         this.displayLoading = false;
       }
       // if not found fetch from api
       else {
         try {
-          fetchedScreen = await localhost.Screen.view(id);
+          let resp = await localhost.Screen.screens(targetId);
           runInAction(() => {
-            console.log("screenStore: fetchScreen fetched from api");
-            console.log(this.selectedScreen);
-            this.selectedScreen = fetchedScreen;
-            this.screenRegistryExpanded.set(id, fetchedScreen);
+            console.log("screenStore: fetchScreens fetched from api");
+            this.screenRegistry.set(targetId, resp);
+            this.selectedScreenedTarget = resp;
           });
         } catch (error) {
           console.log(error);
         } finally {
           runInAction(() => {
             this.displayLoading = false;
-            console.log("screenStore: fetchScreen Complete");
+            console.log("screenStore: fetchScreens Complete");
           });
         }
       }
     };
   
-    get screen() {
+    get screens() {
       console.log("From Screen Store");
-      console.log(this.selectedScreen);
-      return this.selectedScreen;
+      console.log(this.screenRegistry.get(this.selectedScreenedTarget));
+      return this.screenRegistry.get(this.selectedScreenedTarget);
     }
   
-    /* Screen History */
-  
-    fetchScreenHistory = async () => {
-      console.log("screenStore: fetchScreenHistory Start");
-      this.historyDisplayLoading = true;
-      let id = this.selectedScreen.id;
-  
-      // first check cache
-      let fetchedScreenHistory = this.screenHistoryRegistry.get(id);
-      console.log("CACHE");
-      console.log(fetchedScreenHistory);
-      if (fetchedScreenHistory) {
-        console.log("screenStore: fetchedScreenHistory found in cache");
-        this.historyDisplayLoading = false;
-        this.selectedScreenHistory = fetchedScreenHistory;
-      }
-      // if not found fetch from api
-      else {
-        try {
-          fetchedScreenHistory = await localhost.Screen.history(id);
-          runInAction(() => {
-            console.log("screenStore: fetchScreenHistory fetched from api");
-            console.log(fetchedScreenHistory);
-  
-            this.screenHistoryRegistry.set(id, fetchedScreenHistory);
-            this.selectedScreenHistory = fetchedScreenHistory;
-          });
-        } catch (error) {
-          console.log(error);
-        } finally {
-          runInAction(() => {
-            this.historyDisplayLoading = false;
-            console.log("screenStore: fetchScreenHistory Complete");
-          });
-        }
-      }
-    };
-  
-    get screenHistory() {
-      return this.selectedScreenHistory;
-    }
-  
-    /* End Screen History */
-  
-  
-    editScreen = async () => {
-      console.log("screenStore: editScreen Start");
-      this.displayLoading = true;
-      let updatedScreen = null;
-      console.log(this.selectedScreen);
-      // send to server
-      try {
-        updatedScreen = await localhost.Screen.edit(this.selectedScreen);
-        runInAction(() => {
-          console.log("screenStore: fetchScreen fetched from api");
-          console.log(this.selectedScreen);
-          this.selectedScreen = updatedScreen;
-          this.screenRegistryExpanded.set(updatedScreen.id, updatedScreen);
-          this.screenHistoryRegistry.delete(updatedScreen.id);
-        });
-      } catch (error) {
-        console.log(error);
-      } finally {
-        runInAction(() => {
-          this.displayLoading = false;
-          console.log("screenStore: edit Complete");
-        });
-      }
-    };
-  
-    cancelEditScreen = () => {
-      console.log("screenStore: cancelEditScreen");
-      this.selectedScreen = this.screenRegistryExpanded.get(this.selectedScreen.id);
-    };
   }
   
