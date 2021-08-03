0d08ac25dd98c83358d5d301d63c6ca933c5b486
sidxz <rath.siddhant@outlook.com>
2021-08-03 10:00:18 -0500

    moved secrets to env

--- src/services/AppSettingsService.jsx
+++ src/services/AppSettingsService.jsx
@@ -1,36 +1,34 @@
-import msalConfiguration from "../secrets/ms-config.json"
-
 class AppSettingsService {
-  GetWebApiBaseUri()  {
-      return msalConfiguration.REACT_APP_WEB_API_BASE_URI;
+  GetWebApiBaseUri() {
+    return process.env.REACT_APP_WEB_API_BASE_URI;
   }
   GetMsalClientId() {
-      return msalConfiguration.REACT_APP_MSAL_CLIENT_ID;
+    return process.env.REACT_APP_MSAL_CLIENT_ID;
   }
   GetMsalClientScope() {
-      return msalConfiguration.REACT_APP_MSAL_CLIENT_SCOPE;
+    return process.env.REACT_APP_MSAL_CLIENT_SCOPE;
   }
   GetMsalTenantAuthorityUri() {
-      return msalConfiguration.REACT_APP_MSAL_TENANT_AUTHORITY_URI;
+    return process.env.REACT_APP_MSAL_TENANT_AUTHORITY_URI;
   }
   GetMsalCacheLocation() {
-      return msalConfiguration.REACT_APP_MSAL_CACHE_LOCATION;
+    return process.env.REACT_APP_MSAL_CACHE_LOCATION;
   }
   GetMsalStoreAuthInCookie() {
-      let stringValue = msalConfiguration.REACT_APP_MSAL_AUTH_STATE_IN_COOKIE;
+    let stringValue = process.env.REACT_APP_MSAL_AUTH_STATE_IN_COOKIE;
 
-      if (stringValue.toLowerCase() === 'true') {
-          return true;
-      }
-      else if (stringValue.toLowerCase() === 'false') {
-          return false;
-      }
-      else {
-          throw new Error('MSAL_AUTH_STATE_IN_COOKIE setting is not a valid boolean.');
-      }
+    if (stringValue.toLowerCase() === "true") {
+      return true;
+    } else if (stringValue.toLowerCase() === "false") {
+      return false;
+    } else {
+      throw new Error(
+        "MSAL_AUTH_STATE_IN_COOKIE setting is not a valid boolean."
+      );
+    }
   }
   GetLoginRedirectUri() {
-      return msalConfiguration.REACT_APP_MSAL_LOGIN_REDIRECT_URI;
+    return process.env.REACT_APP_MSAL_LOGIN_REDIRECT_URI;
   }
 }
 
