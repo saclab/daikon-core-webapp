78b61278a1913644455d99015186669e3bc196f2
panda-sas <panda.sas20@gmail.com>
2021-08-26 14:24:30 -0500

    Added styling to Gene promote summary

--- src/scenes/Gene/GenomePromote/GenePromoteSummary/GenePromoteSummary.js
+++ src/scenes/Gene/GenomePromote/GenePromoteSummary/GenePromoteSummary.js
@@ -38,7 +38,9 @@ const GenePromoteSummary = (props) => {
 
       <div>
         <div className="card">
-          <h4>Impact of chemical inhibition</h4>
+        
+          <h4 >Impact of chemical inhibition</h4>
+         
           <h5>a) During infections</h5>
 
           <GenePromoteSummaryAnswers
@@ -144,21 +146,183 @@ const GenePromoteSummary = (props) => {
           <Divider />
         </div>
         <div className="card">
-          <h5>Chemical inhibition</h5>
-          <p></p>
+          <h4>Chemical inhibition</h4>
+          <h5>a) in live Mtb</h5>
+          <GenePromoteSummaryAnswers
+            oKey="3a1"
+            questionObj={props.promotionQuestionsRegistry}
+            ansObj={props.targetPromotionFormValue}
+          />
+          <br />
+
+          <GenePromoteSummaryAnswers
+            oKey="3a2"
+            questionObj={props.promotionQuestionsRegistry}
+            ansObj={props.targetPromotionFormValue}
+          />
+          <br />
+          <GenePromoteSummaryAnswers
+            oKey="3a3"
+            questionObj={props.promotionQuestionsRegistry}
+            ansObj={props.targetPromotionFormValue}
+          />
+          <br />
+          <GenePromoteSummaryAnswers
+            oKey="3a4"
+            questionObj={props.promotionQuestionsRegistry}
+            ansObj={props.targetPromotionFormValue}
+          />
+          <br />
+
+          <h5>b) in vitro</h5>
+          <GenePromoteSummaryAnswers
+            oKey="3b1"
+            questionObj={props.promotionQuestionsRegistry}
+            ansObj={props.targetPromotionFormValue}
+          />
+          <br />
+
+          <GenePromoteSummaryAnswers
+            oKey="3b2"
+            questionObj={props.promotionQuestionsRegistry}
+            ansObj={props.targetPromotionFormValue}
+          />
+          <br />
 
           <Divider />
         </div>
         <div className="card">
-          <h5>Impact of genetic inhibition</h5>
-          <p></p>
+          <h4>Impact of genetic inhibition</h4>
+          <h5>a) During infections</h5>
 
+          <GenePromoteSummaryAnswers
+            oKey="4a1"
+            questionObj={props.promotionQuestionsRegistry}
+            ansObj={props.targetPromotionFormValue}
+          />
+          <br />
+
+          <GenePromoteSummaryAnswers
+            oKey="4a2a"
+            questionObj={props.promotionQuestionsRegistry}
+            ansObj={props.targetPromotionFormValue}
+          />
+          <br />
+          <GenePromoteSummaryAnswers
+            oKey="4a2b"
+            questionObj={props.promotionQuestionsRegistry}
+            ansObj={props.targetPromotionFormValue}
+          />
+          <br />
+
+          <GenePromoteSummaryAnswers
+            oKey="4a3a"
+            questionObj={props.promotionQuestionsRegistry}
+            ansObj={props.targetPromotionFormValue}
+          />
+          <br />
+          <GenePromoteSummaryAnswers
+            oKey="4a3b"
+            questionObj={props.promotionQuestionsRegistry}
+            ansObj={props.targetPromotionFormValue}
+          />
+          <br />
+
+          <GenePromoteSummaryAnswers
+            oKey="4a4"
+            questionObj={props.promotionQuestionsRegistry}
+            ansObj={props.targetPromotionFormValue}
+          />
+          <br />
+
+          <h5>b) on replication Mtb in vitro</h5>
+          <GenePromoteSummaryAnswers
+            oKey="4b1"
+            questionObj={props.promotionQuestionsRegistry}
+            ansObj={props.targetPromotionFormValue}
+          />
+          <br />
+
+          <GenePromoteSummaryAnswers
+            oKey="4b2"
+            questionObj={props.promotionQuestionsRegistry}
+            ansObj={props.targetPromotionFormValue}
+          />
+          <br />
+          <GenePromoteSummaryAnswers
+            oKey="4b3"
+            questionObj={props.promotionQuestionsRegistry}
+            ansObj={props.targetPromotionFormValue}
+          />
+          <br />
+
+          <h5>c) on nonreplicating Mtb in vitro</h5>
+          <GenePromoteSummaryAnswers
+            oKey="4c1"
+            questionObj={props.promotionQuestionsRegistry}
+            ansObj={props.targetPromotionFormValue}
+          />
+          <br />
+
+          <GenePromoteSummaryAnswers
+            oKey="4c2"
+            questionObj={props.promotionQuestionsRegistry}
+            ansObj={props.targetPromotionFormValue}
+          />
+          <br />
+          <GenePromoteSummaryAnswers
+            oKey="4c3"
+            questionObj={props.promotionQuestionsRegistry}
+            ansObj={props.targetPromotionFormValue}
+          />
+          <br />
+          <GenePromoteSummaryAnswers
+            oKey="4c4"
+            questionObj={props.promotionQuestionsRegistry}
+            ansObj={props.targetPromotionFormValue}
+          />
+          <br />
+          <GenePromoteSummaryAnswers
+            oKey="4c5"
+            questionObj={props.promotionQuestionsRegistry}
+            ansObj={props.targetPromotionFormValue}
+          />
+          <br />
           <Divider />
         </div>
-        <div className="card">
-          <h5>Liabilities</h5>
-          <p></p>
 
+        <div className="card">
+          <h4>Liabilities</h4>
+          <h5>Metabolic liabilities</h5>
+          <GenePromoteSummaryAnswers
+            oKey="5a1"
+            questionObj={props.promotionQuestionsRegistry}
+            ansObj={props.targetPromotionFormValue}
+          />
+          <br />
+
+          <GenePromoteSummaryAnswers
+            oKey="5a2"
+            questionObj={props.promotionQuestionsRegistry}
+            ansObj={props.targetPromotionFormValue}
+          />
+          <br />
+          <Divider />
+          <h5>Genetic</h5>
+          <GenePromoteSummaryAnswers
+            oKey="5a3"
+            questionObj={props.promotionQuestionsRegistry}
+            ansObj={props.targetPromotionFormValue}
+          />
+          <br />
+          <Divider />
+          <h5>Other</h5>
+          <GenePromoteSummaryAnswers
+            oKey="5b1"
+            questionObj={props.promotionQuestionsRegistry}
+            ansObj={props.targetPromotionFormValue}
+          />
+          <br />
           <Divider />
         </div>
       </div>
