5758c9e9ea5c78b26ef302d8fea8b09e4e24ad79
panda-sas <panda.sas20@gmail.com>
2021-09-07 12:30:34 -0500

    Added fetchTargetAdmin function in the rootstore.

--- src/app/stores/targetStoreAdmin.js
+++ src/app/stores/targetStoreAdmin.js
@@ -10,34 +10,58 @@ import localhost from "../api/localhost";
 export default class TargetStoreAdmin {
   rootStore;
   displayLoading = false;
-  targetRegistry = new Map();
+  targetRegistryAdmin = new Map();
+  selectedTarget = null;
 
   constructor(rootStore) {
     this.rootStore = rootStore;
     makeObservable(this, {
       displayLoading: observable,
-      fetchTargetList: action,
-      targetRegistry: observable,
+      targetRegistryAdmin: observable,
+      fetchTargetAdmin: action,
+      selectedTarget: observable,
     });
   }
-  fetchTargetList = async () => {
-    console.log("targetStoreAdmin: fetchTargetList() Start");
+  /* Fetch specific Target with id from API */
+
+  fetchTargetAdmin = async (id) => {
+    console.log("targetStoreAdmin: fetchTargetAdmin Start");
     this.displayLoading = true;
-    try {
-      var resp = await localhost.Target.list();
-      runInAction(() => {
-        console.log(resp);
-        resp.forEach((fetchedTarget) => {
-          this.targetRegistry.set(fetchedTarget.id, fetchedTarget);
+
+    // first check cache
+    let fetchedTargetAdmin = this.targetRegistryAdmin.get(id);
+    console.log("CACHE");
+    console.log(fetchedTargetAdmin);
+
+    if (fetchedTargetAdmin) {
+      console.log("targetStoreAdmin: fetchedTargetAdmin found in cache");
+      this.selectedTarget = fetchedTargetAdmin;
+      this.displayLoading = false;
+    }
+    // if not found fetch from api
+    else {
+      try {
+        fetchedTargetAdmin = await localhost.TargetAdmin.details(id);
+        runInAction(() => {
+          console.log("targetStoreAdmin: fetchTargetAdmin fetched from api");
+
+          this.selectedTarget = fetchedTargetAdmin;
+          this.targetRegistryAdmin.set(id, fetchedTargetAdmin);
         });
-        console.log(this.targetRegistry);
-      });
-    } catch (error) {
-      console.log(error);
-    } finally {
-      runInAction(() => {
-        this.displayLoading = false;
-      });
+      } catch (error) {
+        console.log(error);
+      } finally {
+        runInAction(() => {
+          this.displayLoading = false;
+          console.log("targetStoreAdmin: fetchTargetAdmin Complete");
+        });
+      }
     }
   };
+
+  get target() {
+    console.log("From Target Store Admin");
+    console.log(this.selectedTarget);
+    return this.selectedTarget;
+  }
 }
--- src/scenes/Admin/TargetAdmin/TargetAdminEditDetails/TargetAdminEditDetails.js
+++ src/scenes/Admin/TargetAdmin/TargetAdminEditDetails/TargetAdminEditDetails.js
@@ -8,20 +8,23 @@ import { TabView, TabPanel } from "primereact/tabview";
 import SectionHeading from "../../../../app/common/SectionHeading/SectionHeading";
 import NotFound from "../../../../app/layout/NotFound/NotFound";
 import { observer } from "mobx-react-lite";
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
 
 const TargetAdminEditDetails = ({ match, history }) => {
   const toast = useRef(null);
   const rootStore = useContext(RootStoreContext);
-  const { fetchTarget, target, displayLoading } = rootStore.targetStore;
+  const { fetchTargetAdmin, target, displayLoading } = rootStore.targetStoreAdmin;
+
 
   useEffect(() => {
     console.log("EFFECT");
     console.log(match.params.id);
 
     if (target === null) {
-      fetchTarget(match.params.id);
+        fetchTargetAdmin(match.params.id);
     }
-  }, [match.params.id, target, fetchTarget]);
+  }, [match.params.id, target, fetchTargetAdmin]);
 
   const items = [
     {
@@ -47,6 +50,7 @@ const TargetAdminEditDetails = ({ match, history }) => {
   if (target !== null) {
     console.log("Target ID");
     console.log(target.id);
+    console.log(target);
     const breadCrumbItems = [
       {
         label: "Target",
@@ -79,10 +83,108 @@ const TargetAdminEditDetails = ({ match, history }) => {
               </div>
               <div className="p-mb-2">
                 <TabView>
-                  <TabPanel></TabPanel>
-                  <TabPanel></TabPanel>
-                  <TabPanel></TabPanel>
+                  <TabPanel style={{ width: "1200px" }}>
+                    <div className="p-field p-grid">
+                      <label
+                        htmlFor="score"
+                        className="p-col-fixed"
+                        style={{ width: "200px" }}
+                      >
+                        Score
+                      </label>
+                      <div className="p-col">
+                        <InputText
+                          id="score"
+                          type="text"
+                          style={{ width: "100px" }}
+                          value={target.score}
+                        />
+                      </div>
+                    </div>
+
+                    <div className="p-field p-grid">
+                      <label
+                        htmlFor="htsfeasibility"
+                        className="p-col-fixed"
+                        style={{ width: "200px" }}
+                      >
+                        HTS Feasibility
+                      </label>
+                      <div className="p-col">
+                        <InputText
+                          id="htsfeasibility"
+                          type="text"
+                          style={{ width: "100px" }}
+                          value={target.htsFeasibility}
+                        />
+                      </div>
+                    </div>
+
+                    <div className="p-field p-grid">
+                      <label
+                        htmlFor="sbdfeasibility"
+                        className="p-col-fixed"
+                        style={{ width: "200px" }}
+                      >
+                        SBD Feasibility
+                      </label>
+                      <div className="p-col">
+                        <InputText
+                          id="sbdfeasibility"
+                          type="text"
+                          style={{ width: "100px" }}
+                          value={target.sbdFeasibility}
+                        />
+                      </div>
+                    </div>
+
+                    <div className="p-field p-grid">
+                      <label
+                        htmlFor="progressibility"
+                        className="p-col-fixed"
+                        style={{ width: "200px" }}
+                      >
+                        Progressibility
+                      </label>
+                      <div className="p-col">
+                        <InputText
+                          id="progressibility"
+                          type="text"
+                          style={{ width: "100px" }}
+                          value={target.progressibility}
+                        />
+                      </div>
+                    </div>
+
+                    <div className="p-field p-grid">
+                      <label
+                        htmlFor="safety"
+                        className="p-col-fixed"
+                        style={{ width: "200px" }}
+                      >
+                        Safety
+                      </label>
+                      <div className="p-col">
+                        <InputText
+                          id="safety"
+                          type="text"
+                          style={{ width: "100px" }}
+                          value={target.safety}
+                        />
+                      </div>
+                    </div>
+                  </TabPanel>
+
                 </TabView>
+
+                
+                <div className="p-d-flex p-jc-end">
+                    <Button
+                      label="Submit"
+                      className="p-button-primary"
+                      icon="pi pi-check"
+                    />
+                  </div>
               </div>
             </div>
           </div>
