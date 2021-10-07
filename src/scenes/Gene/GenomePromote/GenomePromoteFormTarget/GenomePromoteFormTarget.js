bddd3aee604035a8cf2d040ce222a7caa9bac696
panda-sas <panda.sas20@gmail.com>
2021-10-07 10:52:07 -0500

    Created Tractability promotion section

--- src/scenes/Gene/GenomePromote/GenomePromoteFormTarget/GenomePromoteFormTarget.js
+++ src/scenes/Gene/GenomePromote/GenomePromoteFormTarget/GenomePromoteFormTarget.js
@@ -1,27 +1,16 @@
-import React, { useState } from "react";
-import { InputText } from "primereact/inputtext";
-import { SplitButton } from "primereact/splitbutton";
+import React from "react";
 import Question from "../../../../app/common/Question/Question";
+import { SplitButton } from "primereact/splitbutton";
 
 const GenomePromoteFormTarget = (props) => {
-  const [formValue, setFormValue] = useState({
-    geneName: "",
-    associatedMoleculeName: "",
-    accessionNumber: "",
-    addedBy: "",
-    supportingInformation: "",
-    modifedOn: "",
-    modifedBy: "",
-  });
-
-  const setForm = (e) => {
-    var newFormValue = { ...formValue };
-    newFormValue[e.target.name] = e.target.value;
-    setFormValue(newFormValue);
-    console.log(e);
-  };
-
   const nextButtonItems = [
+    {
+      label: "Back",
+      icon: "pi pi-arrow-left",
+      command: () => {
+        props.onFormSet(1);
+      },
+    },
     {
       label: "Save form data in browser",
       icon: "pi pi-cloud-download",
@@ -37,119 +26,218 @@ const GenomePromoteFormTarget = (props) => {
       },
     },
   ];
+
   return (
-    <div className="card">
-      {/* <h5>Horizontal and Fluid</h5> */}
-      <div className="p-fluid">
-        <div className="p-field p-grid">
-          <label htmlFor="geneName" className="p-col-12 p-md-2">
-            Gene name
-          </label>
-          <div className="p-col-12 p-md-10">
-            <InputText
-              id="geneName"
-              name="geneName"
-              type="text"
-              value="rho"
-              disabled
+    <React.Fragment>
+      <div className="card">
+        <h3>a) Sub module 1</h3>
+        <hr />
+        <div className="p-fluid">
+          <div className="p-field p-grid">
+            <Question
+              question={props.promotionQuestionsRegistry.get("6a1")}
+              updateObject={(e) => props.updateTargetPromotionFormValue(e)}
+              readObject={props.targetPromotionFormValue}
             />
           </div>
-        </div>
 
-        <div className="p-field p-grid">
-          <Question
-            question={props.promotionQuestionsRegistry.get("2a3a")}
-            updateObject={(e) => props.updateTargetPromotionFormValue(e)}
-            readObject={props.targetPromotionFormValue}
-          />
-        </div>
-
-        <div className="p-field p-grid">
-          <label htmlFor="accessionNumber" className="p-col-12 p-md-2">
-            Accession Number
-          </label>
-          <div className="p-col-12 p-md-10">
-            <InputText
-              id="accessionNumber"
-              name="accessionNumber"
-              type="text"
-              value="Rv1297"
-              disabled
+          <div className="p-field p-grid">
+            <Question
+              question={props.promotionQuestionsRegistry.get("6a2")}
+              updateObject={(e) => props.updateTargetPromotionFormValue(e)}
+              readObject={props.targetPromotionFormValue}
             />
           </div>
-        </div>
 
-        <div className="p-field p-grid">
-          <label htmlFor="addedBy" className="p-col-12 p-md-2">
-            Added/nominated by
-          </label>
-          <div className="p-col-12 p-md-10">
-            <InputText
-              id="addedBy"
-              name="addedBy"
-              type="text"
-              onChange={(e) => setForm(e)}
+          <div className="p-field p-grid">
+            <Question
+              question={props.promotionQuestionsRegistry.get("6a3")}
+              updateObject={(e) => props.updateTargetPromotionFormValue(e)}
+              readObject={props.targetPromotionFormValue}
             />
           </div>
-        </div>
 
-        <div className="p-field p-grid">
-          <label htmlFor="supportingInformation" className="p-col-12 p-md-2">
-            Supporting Information
-          </label>
-          <div className="p-col-12 p-md-10">
-            <InputText
-              id="supportingInformation"
-              name="supportingInformation"
-              type="text"
-              onChange={(e) => setForm(e)}
+          <div className="p-field p-grid">
+            <Question
+              question={props.promotionQuestionsRegistry.get("6a4")}
+              updateObject={(e) => props.updateTargetPromotionFormValue(e)}
+              readObject={props.targetPromotionFormValue}
             />
           </div>
-        </div>
 
-        <div className="p-field p-grid">
-          <label htmlFor="modifedOn" className="p-col-12 p-md-2">
-            Modifed on
-          </label>
-          <div className="p-col-12 p-md-10">
-            <InputText
-              id="modifedOn"
-              name="modifedOn"
-              type="text"
-              onChange={(e) => setForm(e)}
+          <div className="p-field p-grid">
+            <Question
+              question={props.promotionQuestionsRegistry.get("6a5")}
+              updateObject={(e) => props.updateTargetPromotionFormValue(e)}
+              readObject={props.targetPromotionFormValue}
             />
           </div>
-        </div>
 
-        <div className="p-field p-grid">
-          <label htmlFor="modifedBy" className="p-col-12 p-md-2">
-            Modifed by
-          </label>
-          <div className="p-col-12 p-md-10">
-            <InputText
-              id="modifedBy"
-              name="modifedBy"
-              type="text"
-              onChange={(e) => setForm(e)}
+          <div className="p-field p-grid">
+            <Question
+              question={props.promotionQuestionsRegistry.get("6a6")}
+              updateObject={(e) => props.updateTargetPromotionFormValue(e)}
+              readObject={props.targetPromotionFormValue}
             />
           </div>
-        </div>
 
-        <div className="p-field p-grid p-jc-end">
-          <div className="p-col-12 p-md-2">
-            <SplitButton
-              label="Next"
-              icon="pi pi-arrow-right"
-              model={nextButtonItems}
-              className="p-button-success p-button-sm"
-              onClick={() => {
-                props.onFormSet(formValue);
-              }}
-            ></SplitButton>
+          <div className="p-field p-grid">
+            <Question
+              question={props.promotionQuestionsRegistry.get("6a7")}
+              updateObject={(e) => props.updateTargetPromotionFormValue(e)}
+              readObject={props.targetPromotionFormValue}
+            />
+          </div>
+          <br />
+        </div>
+      </div>
+
+      <div className="card">
+        <h3>b) Sub Module 2</h3>
+        <hr />
+        <div className="p-fluid">
+          <div className="p-field p-grid">
+            <Question
+              question={props.promotionQuestionsRegistry.get("6b1")}
+              updateObject={(e) => props.updateTargetPromotionFormValue(e)}
+              readObject={props.targetPromotionFormValue}
+            />
+          </div>
+          <div className="p-field p-grid">
+            <Question
+              question={props.promotionQuestionsRegistry.get("6b2")}
+              updateObject={(e) => props.updateTargetPromotionFormValue(e)}
+              readObject={props.targetPromotionFormValue}
+            />
+          </div>
+          <div className="p-field p-grid">
+            <Question
+              question={props.promotionQuestionsRegistry.get("6b3")}
+              updateObject={(e) => props.updateTargetPromotionFormValue(e)}
+              readObject={props.targetPromotionFormValue}
+            />
+          </div>
+
+          <div className="p-field p-grid">
+            <Question
+              question={props.promotionQuestionsRegistry.get("6b4")}
+              updateObject={(e) => props.updateTargetPromotionFormValue(e)}
+              readObject={props.targetPromotionFormValue}
+            />
+          </div>
+
+          <div className="p-field p-grid">
+            <Question
+              question={props.promotionQuestionsRegistry.get("6b5")}
+              updateObject={(e) => props.updateTargetPromotionFormValue(e)}
+              readObject={props.targetPromotionFormValue}
+            />
+          </div>
+          <br />
+        </div>
+      </div>
+
+      <div className="card">
+        <h3>c) Sub Module 3</h3>
+        <hr />
+        <div className="p-fluid">
+          <div className="p-field p-grid">
+            <Question
+              question={props.promotionQuestionsRegistry.get("6c1")}
+              updateObject={(e) => props.updateTargetPromotionFormValue(e)}
+              readObject={props.targetPromotionFormValue}
+            />
+          </div>
+          <div className="p-field p-grid">
+            <Question
+              question={props.promotionQuestionsRegistry.get("6c2")}
+              updateObject={(e) => props.updateTargetPromotionFormValue(e)}
+              readObject={props.targetPromotionFormValue}
+            />
+          </div>
+          <div className="p-field p-grid">
+            <Question
+              question={props.promotionQuestionsRegistry.get("6c3")}
+              updateObject={(e) => props.updateTargetPromotionFormValue(e)}
+              readObject={props.targetPromotionFormValue}
+            />
+          </div>
+          <div className="p-field p-grid">
+            <Question
+              question={props.promotionQuestionsRegistry.get("6c4")}
+              updateObject={(e) => props.updateTargetPromotionFormValue(e)}
+              readObject={props.targetPromotionFormValue}
+            />
+          </div>
+          <div className="p-field p-grid">
+            <Question
+              question={props.promotionQuestionsRegistry.get("6c5")}
+              updateObject={(e) => props.updateTargetPromotionFormValue(e)}
+              readObject={props.targetPromotionFormValue}
+            />
+          </div>
+
+          <div className="p-field p-grid">
+            <Question
+              question={props.promotionQuestionsRegistry.get("6c6")}
+              updateObject={(e) => props.updateTargetPromotionFormValue(e)}
+              readObject={props.targetPromotionFormValue}
+            />
+          </div>
+          <br />
+        </div>
+      </div>
+
+      <div className="card">
+        <h3>d) Sub Module 4</h3>
+        <hr />
+        <div className="p-fluid">
+          <div className="p-field p-grid">
+            <Question
+              question={props.promotionQuestionsRegistry.get("6d1")}
+              updateObject={(e) => props.updateTargetPromotionFormValue(e)}
+              readObject={props.targetPromotionFormValue}
+            />
+          </div>
+          <div className="p-field p-grid">
+            <Question
+              question={props.promotionQuestionsRegistry.get("6d2")}
+              updateObject={(e) => props.updateTargetPromotionFormValue(e)}
+              readObject={props.targetPromotionFormValue}
+            />
+          </div>
+          <div className="p-field p-grid">
+            <Question
+              question={props.promotionQuestionsRegistry.get("6d3")}
+              updateObject={(e) => props.updateTargetPromotionFormValue(e)}
+              readObject={props.targetPromotionFormValue}
+            />
+          </div>
+          <div className="p-field p-grid">
+            <Question
+              question={props.promotionQuestionsRegistry.get("6d4")}
+              updateObject={(e) => props.updateTargetPromotionFormValue(e)}
+              readObject={props.targetPromotionFormValue}
+            />
+          </div>
+
+          <br />
+          <div className="p-field p-grid p-jc-end">
+            <div className="p-col-12 p-md-2">
+              <SplitButton
+                label="Next"
+                icon="pi pi-arrow-right"
+                model={nextButtonItems}
+                className="p-button-success p-button-sm"
+                onClick={() => {
+                  props.onFormSet(3);
+                }}
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 };
 
