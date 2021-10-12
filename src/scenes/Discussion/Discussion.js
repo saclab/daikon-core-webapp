ef1645437e83f7f112b74367650bb6cf71c0c9c6
panda-sas <panda.sas20@gmail.com>
2021-10-12 18:36:08 -0500

    Created Comments components

---
+++ src/scenes/Discussion/Discussion.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { Fieldset } from "primereact/fieldset";
+import { Button } from "primereact/button";
+import StartDiscussion from "./StartDiscussion";
+
+
+import { Dialog } from "primereact/dialog";
+
+const Discussion = () => {
+  const [displayDiscussionDialog, setDisplayDiscussionDialog] = useState(false);
+
+  const displayAllDiscussions = () => {
+    setDisplayDiscussionDialog(true);
+  };
+
+  let startNewDiscussion = () => {
+    return (
+      <React.Fragment>
+        <i className="pi pi-comments"></i> &nbsp; Start a new
+        discussion
+      </React.Fragment>
+    );
+  };
+
+  return (
+    <React.Fragment>
+      <Button
+        label="Start a discussion"
+        className="p-button-outlined p-button-secondary p-button-text"
+        icon="pi pi-plus"
+        onClick={displayAllDiscussions}
+      />
+
+      <Dialog
+        header={startNewDiscussion()}
+        visible={displayDiscussionDialog}
+        style={{ width: "50vw" }}
+        maximizable
+        onHide={() => setDisplayDiscussionDialog(false)}
+      >
+        <StartDiscussion />
+      </Dialog>
+
+      <div className="card">
+        <Fieldset legend="Discussion board">
+          <p></p>
+        </Fieldset>
+
+        <br />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Discussion;
