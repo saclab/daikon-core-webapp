384a7f860ee3ff70a1ab0b20aa92bdf00e7f7d12
panda-sas <panda.sas20@gmail.com>
2021-10-12 19:10:42 -0500

    Added styling components to Discussion

--- src/scenes/Discussion/StartDiscussion.js
+++ src/scenes/Discussion/StartDiscussion.js
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 import { InputTextarea } from "primereact/inputtextarea";
 import { Button } from "primereact/button";
+
 const StartDiscussion = () => {
-  const [value1, setValue1] = useState("");
+  const [addComment, setAddComment] = useState("");
 
   return (
     <div>
       <div className="card">
         <InputTextarea
-          value={value1}
-          onChange={(e) => setValue1(e.target.value)}
+          value={addComment}
+          onChange={(e) => setAddComment(e.target.value)}
           rows={5}
           cols={30}
           autoResize
