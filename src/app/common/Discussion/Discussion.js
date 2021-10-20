c66b5a8ebd60d6aa167d52b4c5b5264260e89a81
sidxz <rath.siddhant@outlook.com>
2021-10-20 17:02:37 -0500

    Added comment box

--- src/app/common/Discussion/Discussion.js
+++ src/app/common/Discussion/Discussion.js
@@ -1,34 +1,43 @@
 import React, { useState, useEffect, useContext, useRef } from "react";
+import dateFormat, { masks } from "dateformat";
+
 import { Fieldset } from "primereact/fieldset";
 import { Button } from "primereact/button";
 import { observer } from "mobx-react-lite";
 import { Tag } from "primereact/tag";
+import { Divider } from "primereact/divider";
+import { Card } from "primereact/card";
 import { Dialog } from "primereact/dialog";
+import { Avatar } from "primereact/avatar";
+import { InputTextarea } from "primereact/inputtextarea";
 import { RootStoreContext } from "../../stores/rootStore";
 import Loading from "../../layout/Loading/Loading";
 import StartDiscussion from "./StartDiscussion";
 
 const Discussion = ({ reference }) => {
   const rootStore = useContext(RootStoreContext);
-  const { fetchComments, displayLoading, commentList } = rootStore.commentStore;
-
-  console.log("Will fetch comments for:" + reference);
+  const { fetchDiscussions, loadingDiscussions, discussions } =
+    rootStore.discussionStore;
 
   useEffect(() => {
-    fetchComments(reference);
-  }, [fetchComments]);
-
-  console.log(commentList);
+    fetchDiscussions(reference);
+  }, [fetchDiscussions]);
 
   /* local variables */
 
   const dt = useRef(null);
 
   const [displayDiscussionDialog, setDisplayDiscussionDialog] = useState(false);
+  const [userReplyValue, setuserReplyValue] = useState({});
+  const [displayReplyBox, setDisplayReplyBox] = useState({});
 
   const displayAllDiscussions = () => {
     setDisplayDiscussionDialog(true);
   };
+
+  if (loadingDiscussions) {
+    return <Loading />;
+  }
 
   let startNewDiscussion = () => {
     return (
@@ -38,68 +47,121 @@ const Discussion = ({ reference }) => {
     );
   };
 
-  if (displayLoading) {
-    return <Loading />;
-  }
+  let mapReplyValues = (id, value) => {
+    let tempValue = { ...userReplyValue };
+    tempValue[id] = value;
+    setuserReplyValue(tempValue);
+  };
 
-  let formattedComments = commentList.map((comment) => {
-    let additionalComments = <React.Fragment />;
-    if (comment.additionalcomment.length > 0) {
-      additionalComments = comment.additionalcomment.map(
-        (additionalComment) => {
-          return (
-            <div id={additionalComment.id} style={{ marginLeft: "100px" }}>
-              <Tag
-                className="p-mr-2"
-                severity="success"
-                value={additionalComment.postedBy}
-              ></Tag>
-              <Tag
-                className="p-mr-2"
-                severity="warning"
-                value={additionalComment.date}
-              ></Tag>
-              <p style={{ backgroundColor: "#EDEDED" }}>
-                {additionalComment.commentBody}
-              </p>
-            </div>
-          );
-        }
-      );
+  let mapDisplayReplyBox = (id) => {
+    let tempValue = { ...displayReplyBox };
+    tempValue[id] = true;
+    setDisplayReplyBox(tempValue);
+  };
+
+  let subtitleTemplate = (discussion) => {
+    return (
+      <div style={{ fontSize: "smaller", color: "#999999" }}>
+        <Avatar
+          icon="pi pi-user"
+          size="small"
+          style={{ width: "1rem", height: "1rem" }}
+        />{" "}
+        {discussion.postedBy} on{" "}
+        {dateFormat(discussion.timestamp, "dddd, mmmm dS, yyyy, h:MM:ss TT")}
+      </div>
+    );
+  };
+
+  let titleTemplate = (discussion) => {
+    return (
+      <div>
+        {discussion.topic}{" "}
+        <sup>
+          <Tag
+            style={{ background: "#CCCCCC", padding: "0.1rem 0.2rem" }}
+            value={discussion.section}
+          ></Tag>
+        </sup>
+      </div>
+    );
+  };
+
+  let formatteddiscussions = discussions.map((discussion) => {
+    let formattedReplies = <React.Fragment />;
+    if (discussion.replies.length > 0) {
+      formattedReplies = discussion.replies.map((reply) => {
+        return (
+          <div key={reply.id}>
+            <Divider align="left">
+              <div className="p-d-inline-flex p-ai-center">
+                {subtitleTemplate(reply)}
+              </div>
+            </Divider>
+            <p>{reply.body}</p>
+          </div>
+        );
+      });
     }
 
     return (
-      <div id={comment.id}>
-        <Tag
-          className="p-mr-2"
-          severity="success"
-          value={comment.postedBy}
-        ></Tag>
-        <Tag className="p-mr-2" severity="warning" value={comment.date}></Tag>
-        <Tag className="p-mr-2" severity="warning" value={comment.domain}></Tag>
-        <Button
-          icon="pi pi-reply"
-          className="p-button-rounded p-button-secondary p-button-text"
-          label="Reply"
-          style={{ marginLeft: "1000px" }}
-        />
-        <p style={{ backgroundColor: "#EDEDED" }}> {comment.commentBody}</p>
-        {additionalComments}
+      <Card
+        title={titleTemplate(discussion)}
+        subTitle={subtitleTemplate(discussion)}
+        key={discussion.id}
+        style={{ marginTop: "10px" }}
+      >
+        <p>
+          {discussion.body}{" "}
+          <Button
+            label="Reply"
+            className="p-button-link"
+            icon="ri-arrow-left-up-line"
+            style={{ padding: "0rem 0rem" }}
+            onClick={() => mapDisplayReplyBox(discussion.id)}
+          />
+        </p>
+        <Divider align="left" type="dashed">
+          <b>Replies</b>
+        </Divider>
+        <div style={{ marginLeft: "50px" }}>
+          {displayReplyBox[discussion.id] && (
+            <React.Fragment>
+              <InputTextarea
+                value={userReplyValue[discussion.id]}
+                onChange={(e) => mapReplyValues(discussion.id, e.target.value)}
+                rows={2}
+                style={{ width: "100%" }}
+                autoResize
+                autoFocus
+      
+              />
+              <Button
+              className="p-button-rounded"
+                style={{
+                  background: "#28477f",
+                  border: "0px solid #28477f",
+                  padding: "0.4rem 0.6rem",
+                  float: "right",
+                  marginTop : "0.2rem",
+                  fontSize: "small"
+                }}
+                label="Reply"
+                icon="pi pi-reply"
+              />
+            </React.Fragment>
+          )}
+
+          {formattedReplies}
+        </div>
+
         <br />
-      </div>
+      </Card>
     );
   });
 
   return (
     <React.Fragment>
-      <div style={{ float: "right" }}>
-        <Button
-          className="p-button-rounded p-button-info"
-          icon="pi pi-plus"
-          onClick={displayAllDiscussions}
-        />
-      </div>
-      <br />
       <Dialog
         header={startNewDiscussion()}
         visible={displayDiscussionDialog}
@@ -110,11 +172,18 @@ const Discussion = ({ reference }) => {
         <StartDiscussion />
       </Dialog>
 
-      <div className="card">
-        <Fieldset legend="Discussion board">{formattedComments}</Fieldset>
+      <Fieldset legend="Discussion board">
+        <Button
+          className="p-button-rounded p-button-info"
+          icon="pi pi-plus"
+          label="New Topic"
+          onClick={displayAllDiscussions}
+          style={{ background: "#28477f", border: "0px solid #28477f" }}
+        />
+        {formatteddiscussions}
+      </Fieldset>
 
-        <br />
-      </div>
+      <br />
     </React.Fragment>
   );
 };
