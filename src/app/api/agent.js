9974e211021cf6b531a70966ce071410aa49ee6d
Siddhant Rath <sid@tamu.edu>
2021-11-05 16:58:28 -0500

    genestore added apis

--- src/app/api/agent.js
+++ src/app/api/agent.js
@@ -115,6 +115,72 @@ const Gene = {
     ),
   addEssentiality: (geneId, newEssentiality) =>
     requests.post(`/gene/${geneId}/essentiality`, newEssentiality),
+
+  editEssentiality: (geneId, essentialityId, modEssentiality) =>
+    requests.post(
+      `/gene/${geneId}/essentiality/${essentialityId}`,
+      modEssentiality
+    ),
+  addProteinProduction: (geneId, newProteinProduction) =>
+    requests.post(`/gene/${geneId}/proteinproduction`, newProteinProduction),
+
+  editProteinProduction: (geneId, proteinproductionId, modProteinProduction) =>
+    requests.post(
+      `/gene/${geneId}/proteinproduction/${proteinproductionId}`,
+      modProteinProduction
+    ),
+
+  addProteinProductionAssasy: (geneId, newProteinProductionAssasy) =>
+    requests.post(
+      `/gene/${geneId}/proteinactivityassay`,
+      newProteinProductionAssasy
+    ),
+
+  editProteinProductionAssasy: (
+    geneId,
+    proteinProductionAssasyId,
+    modProteinProductionAssasy
+  ) =>
+    requests.post(
+      `/gene/${geneId}/proteinactivityassay/${proteinProductionAssasyId}`,
+      modProteinProductionAssasy
+    ),
+
+  addCRISPRiStrain: (geneId, newCRISPRiStrain) =>
+    requests.post(`/gene/${geneId}/crispristrain`, newCRISPRiStrain),
+
+  editCRISPRiStrain: (geneId, cRISPRiStrainId, modCRISPRiStrain) =>
+    requests.post(
+      `/gene/${geneId}/crispristrain/${cRISPRiStrainId}`,
+      modCRISPRiStrain
+    ),
+
+  addResistanceMutation: (geneId, newResistanceMutation) =>
+    requests.post(`/gene/${geneId}/resistancemutation`, newResistanceMutation),
+  editResistanceMutation: (
+    geneId,
+    resistanceMutationId,
+    modResistanceMutation
+  ) =>
+    requests.post(
+      `/gene/${geneId}/resistancemutation/${resistanceMutationId}`,
+      modResistanceMutation
+    ),
+
+  addUnpublishedStructure: (geneId, newUnpublishedStructure) =>
+    requests.post(
+      `/gene/${geneId}/unpublishedstructures`,
+      newUnpublishedStructure
+    ),
+  editUnpublishedStructure: (
+    geneId,
+    unpublishedStructureId,
+    modUnpublishedStructure
+  ) =>
+    requests.post(
+      `/gene/${geneId}/unpublishedstructures/${unpublishedStructureId}`,
+      modUnpublishedStructure
+    ),
 };
 
 const GeneAdmin = {
