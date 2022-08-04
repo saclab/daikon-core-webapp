68a4e3585266214cb3aa3830a5c9524128937537
Siddhant Rath <sid@tamu.edu>
2022-08-04 14:18:17 -0500

    Loading box Fix

--- src/app/layout/Loading/Loading.js
+++ src/app/layout/Loading/Loading.js
@@ -8,28 +8,46 @@ let Loading = ({ message }) => {
     <React.Fragment>
       <div className="backdrop">
 
-        <div className="Loading">
+        <div className="cursor-wait Loading">
           <div className="LoadingWrap">
-            <div className="LoadingBox">
-              <div className="p-grid">
-                <div className="p-col-3">
-                  <img src={loadingGif} style={{ width: "100px" }} alt={"Loading.."} />
+            <div className="shadow-8 LoadingBox">
+              <div className="flex pb-3">
+
+                <div className="flex">
+                  <div className="flex">
+                    <img src={loadingGif} style={{ width: "100px" }} alt={"Loading.."} />
+                  </div>
+
+                  <div className="inline-block pl-6" style={{ lineHeight: "1em" }}>
+                    <h1 className="Text">Loading...</h1>
+                    <h3 className="Text">Please Wait.</h3>
+                  </div>
+
                 </div>
-                <div className="p-col-6" style={{ lineHeight: "1em" }}>
-                  <h1 className="Text">Loading...</h1>
-                  <h3 className="Text">Please Wait.</h3>
+                <div className="flex">
+
                 </div>
               </div>
+              <div className="flex flex-column">
+                <div className="block">
+                  <ProgressBar mode="indeterminate" />
+                </div>
+                <div className="flex -my-2 justify-content-end">
+                  <h4 className="bg-yellow-500 pl-3 pr-3 Text">{message !== null ? message : "Fetching..."}</h4>
+                </div>
 
 
-              <ProgressBar mode="indeterminate" />
-              <h4 className="Text">{message !== null ? message : "Fetching..."}</h4>
-
+              </div>
             </div>
+
+
+
+
+
           </div>
         </div>
       </div>
-    </React.Fragment>
+    </React.Fragment >
 
   );
 };
