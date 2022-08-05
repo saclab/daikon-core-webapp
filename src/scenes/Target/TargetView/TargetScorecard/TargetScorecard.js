c94244dd6fa7ea903db9ea4351ac67b6ed8497d0
Siddhant Rath <sid@tamu.edu>
2022-08-05 13:22:44 -0500

    Target/ Scorecard fix

--- src/scenes/Target/TargetView/TargetScorecard/TargetScorecard.js
+++ src/scenes/Target/TargetView/TargetScorecard/TargetScorecard.js
@@ -6,6 +6,10 @@ import { observer } from "mobx-react-lite";
 import TargetGrid from "./TargetGrid/TargetGrid";
 import { RootStoreContext } from "../../../../app/stores/rootStore";
 import Loading from "../../../../app/layout/Loading/Loading";
+import { useNavigate } from 'react-router-dom';
+import SectionHeading from '../../../../app/common/SectionHeading/SectionHeading';
+import { BreadCrumb } from 'primereact/breadcrumb';
+import { appColors } from '../../../../colors';
 
 const TargetScorecard = () => {
   const toast = useRef(null);
@@ -23,6 +27,24 @@ const TargetScorecard = () => {
     fetchQuestions();
   }, [fetchQuestions]);
 
+  const navigate = useNavigate();
+
+  const breadCrumbItems = [
+    {
+      label: "Targets",
+      command: () => {
+        navigate("/d/target/");
+      },
+    },
+    {
+      label: selectedTarget.name,
+      command: () => {
+        navigate(`/d/target/${selectedTarget.id}`);
+      }
+    },
+    { label: "Scorecard" },
+  ];
+
   if (questionsLoading) {
     return <Loading />;
   }
@@ -30,42 +52,52 @@ const TargetScorecard = () => {
   return (
     <React.Fragment>
       <Toast ref={toast} />
-      <div className="p-d-flex">
-        <div className="p-mr-2">
-          <div className="p-d-flex p-flex-column">
-            <div className="p-mb-2">
-              <Fieldset legend="Overview">
-                <div className="p-d-flex">
-                  <div className="p-mr-2">
-                    <h3>
-                      <i className="ri-blaze-line"></i> Bucket :{" "}
-                      <b>{selectedTarget.bucket}</b>
-                    </h3>
-                    <div style={{ inlineSize: "900px", overflowWrap: "break-word" }}>
-                      <h4>
-                        <i className="icon icon-conceptual icon-proteins"></i>{" "}
-                        Associated Genes :{" "}
-                        {selectedTarget.targetGenesAccesionNumbers.join(', ')}
-                      </h4>
-                    </div>
-                  </div>
-                  <div className="p-mr-2">
-                    {/* <TargetScorecardPercentDial /> */}
-                  </div>
-                </div>
-              </Fieldset>
-            </div>
-            <div className="p-mb-2">
-              <Fieldset legend="Scorecard">
-                <TargetGrid
-                  questions={questionsRegistry}
-                  target={selectedTarget}
-                />
-              </Fieldset>
-            </div>
+      <div className="flex flex-column w-full">
+
+        <div className="flex w-full pb-2">
+          <BreadCrumb model={breadCrumbItems} />
+        </div>
+
+        <div className="flex w-full">
+          <SectionHeading
+            icon="icon icon-common icon-target"
+            heading={selectedTarget.name}
+            targetName={selectedTarget.name}
+            displayHorizon={true}
+            color={appColors.sectionHeadingBg.target}
+          />
+        </div>
+
+        <div className="flex flex-column gap-3 w-full">
+          <div className="flex w-full">
+            <Fieldset legend="Overview">
+              <h3>
+                <i className="ri-blaze-line"></i> Bucket :{" "}
+                <b>{selectedTarget.bucket}</b>
+              </h3>
+              <div style={{ inlineSize: "900px", overflowWrap: "break-word" }}>
+                <h4>
+                  <i className="icon icon-conceptual icon-proteins"></i>{" "}
+                  Associated Genes :{" "}
+                  {selectedTarget.targetGenesAccesionNumbers.join(', ')}
+                </h4>
+              </div>
+            </Fieldset>
+          </div>
+
+          <div className="flex w-full">
+            <Fieldset legend="Scorecard">
+              <TargetGrid
+                questions={questionsRegistry}
+                target={selectedTarget}
+              />
+            </Fieldset>
           </div>
         </div>
+
+
       </div>
+
     </React.Fragment>
   );
 };
