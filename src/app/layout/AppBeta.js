d4960687d45d2f9a07c818d67415847f5b229b1a
Siddhant Rath <sid@tamu.edu>
2022-08-08 18:43:25 -0500

    class to className

--- src/app/layout/AppBeta.js
+++ src/app/layout/AppBeta.js
@@ -9,15 +9,15 @@ const AppBeta = () => {
         <MenuBar />
       </div>
       <div className='flex ml-6 mr-6 fadein animation-duration-1000'>
-        <div class="card">
+        <div className="card">
           <h5>Vertical</h5>
-          <div class="field">
+          <div className="field">
             <label for="firstname1" className="block">Firstname</label>
-            <InputText id="firstname1" type="text" class="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-full" />
+            <InputText id="firstname1" type="text" className="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-full" />
           </div>
-          <div class="field">
+          <div className="field">
             <label for="lastname1">Lastname</label>
-            <input id="lastname1" type="text" class="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-full" />
+            <input id="lastname1" type="text" className="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-full" />
           </div>
         </div>
       </div>
