8d680a9b34fdad7541cff0ab736db08d3a80e391
Siddhant Rath <sid@tamu.edu>
2022-08-09 17:44:01 -0500

    flex post portfolio view info

--- src/scenes/PostPortfolio/PostPortfolioView/PostPortfolioInformation/PostPortfolioInformation.js
+++ src/scenes/PostPortfolio/PostPortfolioView/PostPortfolioInformation/PostPortfolioInformation.js
@@ -1,41 +1,86 @@
 import React from "react";
 import { Fieldset } from "primereact/fieldset";
+import { useNavigate } from 'react-router-dom';
 import "./ScrollPanel.css";
-import PortfolioInformationGeneralInformation from "./LocalComponents/PostPortfolioInformationGeneralInformation";
-import PortfolioInformationDates from "./LocalComponents/PostPortfolioInformationDates";
 import CompoundEvolutionTimeline from "../../../../app/common/CompoundEvolutionTimeline/CompoundEvolutionTimeline";
-import PortfolioInformationPriority from "./PostPortfolioInformationPriority/PostPortfolioInformationPriority";
+import SectionHeading from '../../../../app/common/SectionHeading/SectionHeading';
+import { BreadCrumb } from 'primereact/breadcrumb';
+import { appColors } from '../../../../colors';
+import PostPortfolioInformationGeneralInformation from './LocalComponents/PostPortfolioInformationGeneralInformation';
+import PostPortfolioInformationDates from './LocalComponents/PostPortfolioInformationDates';
+import PostPortfolioInformationPriority from './PostPortfolioInformationPriority/PostPortfolioInformationPriority';
 
 const PostPortfolioInformation = ({ id, project }) => {
 
+  const navigate = useNavigate();
+
+  const breadCrumbItems = [
+    {
+      label: "Post Portfolio",
+      command: () => {
+        navigate("/d/portfolio/");
+      },
+    },
+    {
+      label: project.projectName,
+      command: () => {
+        navigate(`/d/post-portfolio/${project.id}`);
+      }
+    },
+    { label: "Information" },
+  ];
+
 
   return (
-    <div>
+    <React.Fragment>
       {/* First div for general information and dates */}
-      <div className="p-d-flex p-flex-column p-flex-md-row">
-        <div className="p-mb-2 p-mr-2">
-          <Fieldset legend="Project Information">
-            <PortfolioInformationGeneralInformation project={project} />
-          </Fieldset>
-        </div>
-        <div className="p-mb-2 p-mr-2">
-          <Fieldset legend="Project Dates">
-            <PortfolioInformationDates project={project} />
-          </Fieldset>
-          <br />
-          <Fieldset>
-            <PortfolioInformationPriority project={project} />
-          </Fieldset>
+
+      <div className="flex flex-column gap-2 w-full">
+        <div className="flex w-full pb-2">
+          <BreadCrumb model={breadCrumbItems} />
         </div>
 
+        <div className="flex w-full">
+          <SectionHeading
+            icon="icon icon-common icon-drug"
+            heading={
+              project.projectName +
+              " | " +
+              project?.currentStage
+            }
+            targetName={project.targetName}
+            displayHorizon={true}
+            color={appColors.sectionHeadingBg.portfolio}
+          />
+        </div>
+        <div className="flex w-full">
+          <div className="flex">
+            <Fieldset legend="Project Information">
+              <PostPortfolioInformationGeneralInformation project={project} />
+            </Fieldset>
+          </div>
+          <div className="flex-column w-full">
+            <div className="flex w-full pb-2">
+              <Fieldset className="w-full" legend="Project Dates">
+                <PostPortfolioInformationDates project={project} />
+              </Fieldset>
+            </div>
+            <div className="flex w-full">
+              <Fieldset legend="Project Team P/P">
+                <PostPortfolioInformationPriority project={project} />
+              </Fieldset>
+            </div>
+          </div>
+
+        </div>
+
+        <div className="flex w-full">
+          <Fieldset legend="Compound Evolution">
+            <CompoundEvolutionTimeline project={project} />
+          </Fieldset>
+        </div>
       </div>
-      {/* Second div for structure evolution */}
-      <div className="p-d-flex p-flex-column p-flex-md-row"  >
-        <Fieldset legend="Compound Evolution">
-          <CompoundEvolutionTimeline project={project} />
-        </Fieldset>
-      </div>
-    </div>
+    </React.Fragment>
   );
 };
 
