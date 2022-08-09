f68b12d84429b4b7cfbadf2146d815fdff3a5f2f
Siddhant Rath <sid@tamu.edu>
2022-08-08 19:24:36 -0500

    Flex Portfolio Info

--- src/scenes/Portfolio/PortfolioView/PortfolioInformation/PortfolioInformation.js
+++ src/scenes/Portfolio/PortfolioView/PortfolioInformation/PortfolioInformation.js
@@ -1,41 +1,83 @@
 import React from "react";
 import { Fieldset } from "primereact/fieldset";
+import { useNavigate } from 'react-router-dom';
+
 import "./ScrollPanel.css";
 import PortfolioInformationGeneralInformation from "./LocalComponents/PortfolioInformationGeneralInformation";
 import PortfolioInformationDates from "./LocalComponents/PortfolioInformationDates";
 import CompoundEvolutionTimeline from "../../../../app/common/CompoundEvolutionTimeline/CompoundEvolutionTimeline";
 import PortfolioInformationPriority from "./PortfolioInformationPriority/PortfolioInformationPriority";
+import { BreadCrumb } from 'primereact/breadcrumb';
+import SectionHeading from '../../../../app/common/SectionHeading/SectionHeading';
+import { appColors } from '../../../../colors';
 
 const PortfolioInformation = ({ id, project }) => {
+  const navigate = useNavigate();
+
+  const breadCrumbItems = [
+    {
+      label: "Portfolio",
+      command: () => {
+        navigate("/d/portfolio/");
+      },
+    },
+    {
+      label: project.projectName,
+      command: () => {
+        navigate(`/d/portfolio/${project.id}`);
+      }
+    },
+    { label: "Information" },
+  ];
 
 
   return (
-    <div>
+    <React.Fragment>
       {/* First div for general information and dates */}
-      <div className="p-d-flex p-flex-column p-flex-md-row">
-        <div className="p-mb-2 p-mr-2">
-          <Fieldset legend="Project Information">
-            <PortfolioInformationGeneralInformation project={project} />
+
+      <div className="flex flex-column gap-2 w-full">
+        <div className="flex w-full pb-2">
+          <BreadCrumb model={breadCrumbItems} />
+        </div>
+
+        <div className="flex w-full">
+          <SectionHeading
+            icon="icon icon-common icon-analyse"
+            heading={
+              project.projectName +
+              " | " +
+              project?.currentStage
+            }
+            targetName={project.targetName}
+            displayHorizon={true}
+            color={appColors.sectionHeadingBg.portfolio}
+          />
+        </div>
+        <div className="flex w-full">
+          <div className="flex">
+            <Fieldset legend="Project Information">
+              <PortfolioInformationGeneralInformation project={project} />
+            </Fieldset>
+          </div>
+          <div className="flex">
+            <Fieldset legend="Project Dates">
+              <PortfolioInformationDates project={project} />
+            </Fieldset>
+          </div>
+        </div>
+        <div className="flex w-full">
+          <Fieldset legend="Project Team P/P">
+            <PortfolioInformationPriority project={project} />
           </Fieldset>
         </div>
-        <div className="p-mb-2 p-mr-2">
-          <Fieldset legend="Project Dates">
-            <PortfolioInformationDates project={project} />
-          </Fieldset>
-          <br />
-          <Fieldset>
-            <PortfolioInformationPriority project={project} />
+        <div className="flex w-full">
+          <Fieldset legend="Compound Evolution">
+            <CompoundEvolutionTimeline project={project} />
           </Fieldset>
         </div>
 
       </div>
-      {/* Second div for structure evolution */}
-      <div className="p-d-flex p-flex-column p-flex-md-row"  >
-        <Fieldset legend="Compound Evolution">
-          <CompoundEvolutionTimeline project={project} />
-        </Fieldset>
-      </div>
-    </div>
+    </React.Fragment>
   );
 };
 
