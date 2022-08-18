8a880267171d90a1449488e7ea307f4903807712
Siddhant Rath <sid@tamu.edu>
2022-08-18 08:03:59 -0500

    code cleanup

--- src/scene-pm/ProjectManagerDashboard/ProjectManagerDashboard.js
+++ src/scene-pm/ProjectManagerDashboard/ProjectManagerDashboard.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import SectionHeading from '../../app/common/SectionHeading/SectionHeading';
+import { appColors } from '../../colors';
 
 const ProjectManagerDashboard = () => {
   return (
-    <div>ProjectManagerDashboard</div>
+    <div className="flex flex-column w-full fadein animation-duration-500">
+      <div className="flex w-full">
+        <SectionHeading
+          icon="icon icon-common icon-briefcase"
+          heading="Project Dashboard"
+          color={appColors.sectionHeadingBg.project}
+        />
+      </div>
+    </div>
   )
 }
 
