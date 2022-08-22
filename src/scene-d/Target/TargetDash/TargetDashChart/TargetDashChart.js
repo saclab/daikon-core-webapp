bbc82398f90e75faceacc0f602cc3fb2acddbb32
Saswati Panda <panda.sas20@gmail.com>
2022-08-22 12:18:45 -0500

    Target Dash echart

--- src/scene-d/Target/TargetDash/TargetDashChart/TargetDashChart.js
+++ src/scene-d/Target/TargetDash/TargetDashChart/TargetDashChart.js
@@ -1,113 +1,367 @@
-import React, { useState } from "react";
-import { Slider } from 'primereact/slider';
+import React, { useState, useEffect, useContext } from "react";
+import ReactECharts from "echarts-for-react";
+import * as echarts from "echarts";
+import { observer } from "mobx-react-lite";
+import { Slider } from "primereact/slider";
 import { useNavigate } from "react-router-dom";
-
-//import { ResponsiveScatterPlot } from "@nivo/scatterplot";
 import history from "../../../../history";
-const TargetDashChart = ({ targets }) => {
+import { appColors } from "../../../../colors";
+import { RootStoreContext } from "../../../../app/stores/rootStore";
+import Loading from "../../../../app/layout/Loading/Loading";
 
+const TargetDashChart = ({ targets }) => {
   const navigate = useNavigate();
 
-  let graphData = [];
+  /* MobX Store */
+  const rootStore = useContext(RootStoreContext);
+  const { loadingTargetDash, targetDash, loadTargetDash } =
+    rootStore.dataViewStore;
+
+  useEffect(() => {
+    console.log("AppBeta: fetchTargetList()");
+    if (targetDash === null) loadTargetDash();
+  }, [targetDash, loadTargetDash]); // eslint-disable-line react-hooks/exhaustive-deps
 
   const [likeScoreCutoff, setLikeScoreCutoff] = useState(0.02);
   const [impactScoreCutoff, setImpactScoreCutoff] = useState(0.02);
+  /** Loading Overlay */
+  if (loadingTargetDash) {
+    return <Loading />;
+  }
 
+  let nodeColors = {
+    target: appColors.horizonText.target,
+    screen: appColors.horizonText.screen,
+    fha: appColors.horizonText.fha,
+    portfolio: appColors.horizonText.portfolio,
+    postPortfolio: appColors.horizonText.postPortfolio,
+  };
+
+  var targetData = [];
+  var screenData = [];
+  var fhaData = [];
+  var portfolioData = [];
+  var postPortfolioData = [];
+
+  let showLabel = true;
+
+  if (!loadingTargetDash && targetDash !== null) {
+    targetDash.forEach((element) => {
+      if (
+        element.likeScore >= likeScoreCutoff &&
+        element.impactScore >= impactScoreCutoff
+      ) {
+        if (element.currentStage === "Target") {
+          targetData.push([
+            element.likeScore,
+            element.impactScore,
+            element.id,
+            element.name,
+            element.type,
+            element.bucket,
+            element.currentStage,
+          ]);
+        }
+        if (element.currentStage === "Screen") {
+          screenData.push([
+            element.likeScore,
+            element.impactScore,
+            element.id,
+            element.name,
+            element.type,
+            element.bucket,
+            element.currentStage,
+          ]);
+        }
+        if (element.currentStage === "FHA") {
+          fhaData.push([
+            element.likeScore,
+            element.impactScore,
+            element.id,
+            element.name,
+            element.type,
+            element.bucket,
+            element.currentStage,
+          ]);
+        }
+        if (element.currentStage === "Portfolio") {
+          portfolioData.push([
+            element.likeScore,
+            element.impactScore,
+            element.id,
+            element.name,
+            element.type,
+            element.bucket,
+            element.currentStage,
+          ]);
+        }
+        if (element.currentStage === "PostPortfolio") {
+          postPortfolioData.push([
+            element.likeScore,
+            element.impactScore,
+            element.id,
+            element.name,
+            element.type,
+            element.bucket,
+            element.currentStage,
+          ]);
+        }
+      }
+    });
+
+    let option = {
+      backgroundColor: new echarts.graphic.RadialGradient(0.3, 0.3, 0.8, [
+        {
+          offset: 0,
+          color: "#fff",
+        },
+        {
+          offset: 1,
+          color: "#eee",
+        },
+      ]),
+      title: {
+        text: "Target map",
+        left: "5%",
+        top: "3%",
+      },
+      legend: {
+        right: "10%",
+        top: "3%",
+        data: ["Target", "Screen", "FHA", "Portfolio", "PostPortfolio"],
+      },
+      grid: {
+        left: "8%",
+        top: "10%",
+      },
+      xAxis: {
+        splitLine: {
+          lineStyle: {
+            type: "dashed",
+          },
+        },
+      },
+      yAxis: {
+        splitLine: {
+          lineStyle: {
+            type: "dashed",
+          },
+        },
+        scale: true,
+      },
+      series: [
+        {
+          name: "Target",
+          data: targetData,
+          type: "scatter",
+          label: {
+            formatter: function (param) {
+              return param.data[3];
+            },
+            position: "top",
+            show: showLabel,
+            fontSize: 8,
+          },
+          emphasis: {
+            focus: "series",
+            label: {
+              show: true,
+              formatter: function (param) {
+                return param.data[3];
+              },
+              position: "top",
+            },
+          },
+          itemStyle: {
+            shadowBlur: 10,
+            shadowColor: "rgba(120, 36, 50, 0.5)",
+            shadowOffsetY: 5,
+            color: nodeColors.target,
+          },
+        },
+        {
+          name: "Screen",
+          data: screenData,
+          type: "scatter",
+          label: {
+            formatter: function (param) {
+              return param.data[3];
+            },
+            position: "top",
+            show: showLabel,
+            fontSize: 8,
+          },
+
+          emphasis: {
+            focus: "series",
+            label: {
+              show: true,
+              formatter: function (param) {
+                return param.data[3];
+              },
+              position: "top",
+            },
+          },
+          itemStyle: {
+            shadowBlur: 10,
+            shadowColor: "rgba(25, 100, 150, 0.5)",
+            shadowOffsetY: 5,
+            color: nodeColors.screen,
+          },
+        },
+        {
+          name: "FHA",
+          data: fhaData,
+          type: "scatter",
+          label: {
+            formatter: function (param) {
+              return param.data[3];
+            },
+            position: "top",
+            show: showLabel,
+            fontSize: 8,
+          },
+          emphasis: {
+            focus: "series",
+            label: {
+              show: true,
+              formatter: function (param) {
+                return param.data[3];
+              },
+              position: "top",
+            },
+          },
+          itemStyle: {
+            shadowBlur: 10,
+            shadowColor: "rgba(120, 36, 50, 0.5)",
+            shadowOffsetY: 5,
+            color: nodeColors.fha,
+          },
+        },
+        {
+          name: "Portfolio",
+          data: portfolioData,
+          type: "scatter",
+          label: {
+            formatter: function (param) {
+              return param.data[3];
+            },
+            position: "top",
+            show: showLabel,
+            fontSize: 8,
+          },
+          emphasis: {
+            focus: "series",
+            label: {
+              show: true,
+              formatter: function (param) {
+                return param.data[3];
+              },
+              position: "top",
+            },
+          },
+          itemStyle: {
+            shadowBlur: 10,
+            shadowColor: "rgba(120, 36, 50, 0.5)",
+            shadowOffsetY: 5,
+            color: nodeColors.portfolio,
+          },
+        },
+        {
+          name: "PostPortfolio",
+          data: postPortfolioData,
+          type: "scatter",
+          label: {
+            formatter: function (param) {
+              return param.data[3];
+            },
+            position: "top",
+            show: showLabel,
+            fontSize: 8,
+          },
+          emphasis: {
+            focus: "series",
+            label: {
+              show: true,
+              formatter: function (param) {
+                return param.data[3];
+              },
+              position: "top",
+            },
+          },
+          itemStyle: {
+            shadowBlur: 10,
+            shadowColor: "rgba(120, 36, 50, 0.5)",
+            shadowOffsetY: 5,
+            color: nodeColors.postPortfolio,
+          },
+        },
+      ],
+    };
+
+    console.log(targetDash);
+
+    return (
+      <React.Fragment>
+        <div>
+          <div style={{ height: "650px", width: "650px", marginTop: "20px" }}>
+            <ReactECharts
+              option={option}
+              // onEvents={onEvents}
+              style={{ height: "650px", width: "650px" }}
+            />
+          </div>
+          <div
+            style={{
+              padding: "0px 60px 0px 60px",
+              margin: "-40px 0px 0px 0px",
+            }}
+          >
+            <h4>
+              <i className="icon icon-common icon-filter" /> Filters
+            </h4>
+            <h5>Likelihood Score: {likeScoreCutoff}</h5>
+            <Slider
+              min={0}
+              max={1}
+              step={0.01}
+              value={likeScoreCutoff}
+              onChange={(e) => setLikeScoreCutoff(e.value)}
+            />
+            <h5>Biological Impact Score: {impactScoreCutoff}</h5>
+            <Slider
+              min={0}
+              max={1}
+              step={0.01}
+              value={impactScoreCutoff}
+              onChange={(e) => setImpactScoreCutoff(e.value)}
+            />
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 
   console.log("START generating graphdata");
 
-  targets.forEach((target) => {
+  // targets.forEach((target) => {
+  //   if (
+  //     target.likeScore >= likeScoreCutoff &&
+  //     target.impactScore >= impactScoreCutoff
+  //   ) {
+  //     graphData.push([
+  //       target.likeScore,
+  //       target.impactScore,
+  //       target.name,
+  //       target.id,
+  //     ]);
+  //   }
+  // });
 
-    if ((target.likeScore >= likeScoreCutoff) && (target.impactScore >= impactScoreCutoff)) {
-      graphData.push(
-        [target.likeScore, target.impactScore, target.name, target.id]
-      );
-    }
-
-  });
-
-  console.log(graphData);
+  // console.log(graphData);
   console.log("END generating graphdata");
 
-  const CustomNode = ({ node, onMouseEnter, onMouseMove, onMouseLeave, onClick }) => {
-    return (
-      <React.Fragment>
-        <g transform={`translate(${node.x},${node.y}) rotate(45)`}>
-          <circle
-            r={node.size / 2}
-            fill={node.color}
-            style={{ mixBlendMode: node.blendMode }}
-            onMouseEnter={onMouseEnter}
-            onMouseMove={onMouseMove}
-            onMouseLeave={onMouseLeave}
-            onClick={onClick}
-          />
-
-          <text x="15" y="0" fill="black" fontSize="smaller">
-            {node.id.substring(0, node.id.length - 2)}
-          </text>
-        </g>
-      </React.Fragment>
-    );
-  };
-
-  let nodeClicked = (node, event) => {
-    navigate("./" + node.data.guid);
-  };
-
-  return (
-    <div>
-      <div style={{ height: "650px", width: "650px", marginTop: "10px" }}>
-        {/* <ResponsiveScatterPlot
-          data={graphData}
-          margin={{ top: 0, right: 10, bottom: 80, left: 80 }}
-          xScale={{ type: "linear", min: 0, max: 1 }}
-          yScale={{ type: "linear", min: 0, max: 1 }}
-          blendMode="darken"
-          colors="nivo"
-          axisTop={null}
-          axisRight={null}
-          nodeSize={5}
-          onClick={nodeClicked}
-          axisBottom={{
-            orient: "bottom",
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: "Likelihood Score --->",
-            legendPosition: "middle",
-            legendOffset: 46,
-          }}
-          axisLeft={{
-            orient: "left",
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: "Biological Impact Score --->",
-            legendPosition: "middle",
-            legendOffset: -60,
-          }}
-          layers={[
-            "grid",
-            "axes",
-            "nodes",
-            "markers",
-            "mesh",
-            "legends",
-            "annotations",
-          ]}
-          nodeComponent={CustomNode}
-        /> */}
-      </div>
-      <div style={{ padding: "0px 60px 0px 60px", margin: "-40px 0px 0px 0px" }}>
-        <h4><i className="icon icon-common icon-filter" /> Filters</h4>
-        <h5>Likelihood Score: {likeScoreCutoff}</h5>
-        <Slider min={0} max={1} step={0.01} value={likeScoreCutoff} onChange={(e) => setLikeScoreCutoff(e.value)} />
-        <h5>Biological Impact Score: {impactScoreCutoff}</h5>
-        <Slider min={0} max={1} step={0.01} value={impactScoreCutoff} onChange={(e) => setImpactScoreCutoff(e.value)} />
-      </div>
-    </div>
-  );
+  return <h2>Wait..</h2>;
 };
 
-export default TargetDashChart;
+export default observer(TargetDashChart);
