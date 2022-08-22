7ff9f9ee58710418488bd233a8c2283e87e24319
Siddhant Rath <sid@tamu.edu>
2022-08-22 11:49:21 -0500

    Voting

--- src/scene-d/Screen/ScreenView/ValidatedHits/ValidatedHitsList/ValidatedHitsList.js
+++ src/scene-d/Screen/ScreenView/ValidatedHits/ValidatedHitsList/ValidatedHitsList.js
@@ -14,6 +14,7 @@ import ValidatedHitsImporter from "./ValidatedHitsImporter/ValidatedHitsImporter
 import ValidatedHitsPromoteToFHAEntry from "./ValidatedHitsPromoteToFHAEntry/ValidatedHitsPromoteToFHAEntry";
 import { toast } from "react-toastify";
 import "./ValidatedHitsDataTable.css";
+import { ConfirmDialog } from "primereact/confirmdialog";
 
 const ValidatedHitsList = ({ screenId }) => {
   const dt = useRef(null);
@@ -385,6 +386,7 @@ const ValidatedHitsList = ({ screenId }) => {
           close={() => setDisplayPromoteToFHAEntry(false)}
         />
       </Dialog>
+      <ConfirmDialog />
     </div>
   );
 };
