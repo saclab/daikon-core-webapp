39a91c978d2ca977e5345cff30effa7cfe3bf7b9
Siddhant Rath <sid@tamu.edu>
2022-08-22 14:35:50 -0500

    Notfound center

--- src/app/layout/NotFound/NotFound.js
+++ src/app/layout/NotFound/NotFound.js
@@ -4,7 +4,7 @@ import "./NotFound.css";
 
 const NotFound = () => {
   return (
-    <div>
+    <div className='w-full'>
       <div className="NotFound">
         <div className="NotFoundWrap">
           <div className="NotFoundBox">
