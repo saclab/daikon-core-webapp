b642f4ed2a01af8567e7c2cf9992f788c573fd28
Siddhant Rath <sid@tamu.edu>
2022-08-23 09:33:14 -0500

    Summary Answer reformat

---
+++ src/scene-d/Gene/GenePromote/GenePromoteSummary/GenePromoteSummaryAnswers/GenePromoteSummaryAnswers.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const GenePromoteSummaryAnswers = ({ oKey, questionObj, ansObj }) => {
+
+  console.log(ansObj[oKey]);
+
+  let qClass = ansObj[oKey] === undefined ?
+    "flex align-items-center w-2 pl-2 border-1 border-round border-red-500 surface-overlay" :
+    "flex align-items-center w-2 pl-2 border-1 border-round"
+
+  let dClass = () => {
+    let allowedAns = ['Yes', 'No', 'Active', 'Inactive', 'High', 'Medium', 'Low'];
+    let unallowedAns = ['Unknown']
+
+    if (ansObj[oKey] === undefined) {
+      return "flex align-items-center w-6 pl-2 border-1 border-round border-red-500 surface-overlay"
+    }
+    if (allowedAns.includes(ansObj[oKey]?.answer) && ansObj[oKey]?.description === undefined) {
+      return "flex align-items-center w-6 pl-2 border-1 border-round border-red-500 surface-overlay"
+    }
+    else {
+      return "flex align-items-center w-6 pl-2 border-1 border-round"
+    }
+
+  }
+
+  return (
+    <div className="flex align-content-center gap-2 mb-2">
+      <div className="flex align-items-center w-4 pl-2 border-1 border-round">
+        <p>
+          <b>{oKey} | </b>
+          {questionObj.get(oKey)?.questionBody} :{" "}
+        </p>
+      </div>
+      <div className={qClass}>
+        <p>{ansObj[oKey]?.answer}</p>
+      </div>
+      <div className={dClass()} style={{ overflowWrap: "break-word" }}>
+        <p style={{ overflowWrap: "break-word" }}>{ansObj[oKey]?.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default GenePromoteSummaryAnswers;
