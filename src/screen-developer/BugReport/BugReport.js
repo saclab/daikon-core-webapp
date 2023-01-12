82dd04d034515f351fc3e05898edac92c62e27dc
Siddhant Rath <sid@tamu.edu>
2023-01-12 13:58:16 -0600

    Bug Report

--- src/screen-developer/BugReport/BugReport.js
+++ src/screen-developer/BugReport/BugReport.js
@@ -1,9 +1,26 @@
+import { Button } from "primereact/button";
 import { Editor } from "primereact/editor";
 import React, { useState } from "react";
+import { toast } from "react-toastify";
 import EmbededHelp from "../../app/common/EmbededHelp/EmbededHelp";
+import { _helper_detect_browser_details } from "./BugReportHelper";
 
 const BugReport = () => {
-  const [description, setDescription] = useState("");
+  let prefilledText =
+    `<b>1. Bug Description</b><br /><br />
+    [Enter bug description/details here]
+    <br />
+    <br />
+    <b>2. Steps to reproduce</b><br /><br />
+    [Enter the steps to reproduce the bug]
+    <br />
+    <br />
+    <p>
+      <b>3. Captured Browser Details</b>` +
+    _helper_detect_browser_details() +
+    `</p>
+  `;
+  const [description, setDescription] = useState(prefilledText);
 
   const headerOfTextEditor = (
     <span className="ql-formats">
@@ -16,26 +33,57 @@ const BugReport = () => {
     </span>
   );
 
+  let copyTemplate = () => {
+    navigator.clipboard.writeText(description);
+    toast.success(
+      "Copied text to clipboard. Now please visit GitHub to record the issue."
+    );
+  };
+
   return (
     <div className="flex w-full flex-column pl-6">
-      <h2>Report a bug.</h2>
+      <h2>Bug Report Template</h2>
       <EmbededHelp>
         Bugs are maintained in github and are <b>&nbsp; public</b>. Make sure
         you <b>&nbsp; do not have any sensitive data &nbsp;</b> in the bug
         report.
       </EmbededHelp>
       <p>
-        For your convinence we have pre filled basic information such as browser
-        details, os etc. to the template. Please <b> add</b> the
-        <b> bug description</b> and the <b>steps to reproduce</b>.
+        - Please <b> fill</b> the <b> 'bug description'</b> and the{" "}
+        <b>'steps to reproduce'</b> sections. <br />- click on 'COPY Text'
+        button to copy the filled template.
+        <br />- Click on 'Open GitHub Issues Page' give a title, paste the
+        content to comments, and create an issue.
       </p>
-
+      <div className="flex">
+        <div className="flex pr-2 pb-2">
+          <Button
+            label="COPY Text"
+            className="p-button-secondary"
+            onClick={() => copyTemplate()}
+          />
+        </div>
+        <div className="flex pr-2 pb-2">
+          <Button
+            label="Open GitHub Issues Page"
+            className="p-button-secondary"
+            onClick={() =>
+              window.open(
+                "https://github.com/saclab/daikon-core-webapp/issues/new",
+                "_blank",
+                "noreferrer"
+              )
+            }
+          />
+        </div>
+      </div>
       <Editor
         style={{ height: "25rem", width: "100%" }}
         headerTemplate={headerOfTextEditor}
         value={description}
         onTextChange={(e) => {
           setDescription(e.htmlValue);
+          console.log(e.htmlValue);
         }}
       />
     </div>
