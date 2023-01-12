b5c1b8c68fb5bec9bda18fb12c894cce7b31ebe7
Siddhant Rath <sid@tamu.edu>
2023-01-12 14:58:39 -0600

    Data Disc

--- src/screen-developer/DataDiscrepancy/DataDiscrepancy.js
+++ src/screen-developer/DataDiscrepancy/DataDiscrepancy.js
@@ -1,7 +1,15 @@
 import React from "react";
+import EmbededHelp from "../../app/common/EmbededHelp/EmbededHelp";
 
 const DataDiscrepancy = () => {
-  return <div>DataDiscrepancy</div>;
+  return (
+    <div className="flex w-full flex-column pl-6">
+      <h2>Data Discrepancy Template</h2>
+      <EmbededHelp>
+        Thank you for your time! Feedbacks helps us improve.
+      </EmbededHelp>
+    </div>
+  );
 };
 
 export default DataDiscrepancy;
