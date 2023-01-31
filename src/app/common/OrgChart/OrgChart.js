9dcea07f5b598264b4da3357f8c1b846afdb78a4
Siddhant Rath <sid@tamu.edu>
2023-01-31 12:54:54 -0600

    General Tag Component

--- src/app/common/OrgChart/OrgChart.js
+++ src/app/common/OrgChart/OrgChart.js
@@ -1,29 +1,69 @@
-import React, { useContext, useEffect } from "react";
+import { ContextMenu } from "primereact/contextmenu";
+import React, { useContext, useRef, useState } from "react";
 import { RootStoreContext } from "../../stores/rootStore";
 
-import PleaseWait from "../PleaseWait/PleaseWait";
+import { Button } from "primereact/button";
+import { Dialog } from "primereact/dialog";
+import { SelectButton } from "primereact/selectbutton";
 
 const OrgChart = ({ activeOrgs, primary }) => {
   console.log(activeOrgs);
   const rootStore = useContext(RootStoreContext);
   const { fetchingAppVars, appVars, fetchAppVars } = rootStore.generalStore;
-
-  useEffect(() => {
-    if (appVars === null) {
-      fetchAppVars();
-    }
-  }, [appVars, fetchAppVars]);
-
-  if (fetchingAppVars) {
-    return <PleaseWait />;
-  }
-  console.log(appVars);
+  const cm = useRef(null);
+  const [displayEditContainer, setDisplayEditContainer] = useState(false);
 
   let flattenActiveOrgs = [
     ...activeOrgs.map((activeOrg) => activeOrg.appOrg.alias),
   ];
-  console.log("Flattened activeOrgs");
-  console.log(flattenActiveOrgs);
+
+  let formattedActiveOrgs = [
+    ...activeOrgs.map((activeOrg) => activeOrg.appOrg),
+  ];
+  const [edittedOrgs, setEdittedOrgs] = useState(formattedActiveOrgs);
+
+  const contextMenuItems = [
+    {
+      label: "Edit Membership",
+      icon: "pi pi-tablet",
+      command: () => setDisplayEditContainer(true),
+    },
+  ];
+
+  let editOrgs = () => {
+    console.log(edittedOrgs);
+  };
+
+  let headerEditDialog = () => (
+    <React.Fragment>
+      <i className="icon icon-common icon-database"></i> &nbsp; Editing
+      Participating Organization
+    </React.Fragment>
+  );
+
+  let footerEditDialog = () => (
+    <div>
+      <h3>Save changes to database?</h3>
+      <Button
+        label="Cancel"
+        icon="pi pi-times"
+        onClick={() => {
+          setEdittedOrgs(formattedActiveOrgs);
+          setDisplayEditContainer(false);
+        }}
+        className="p-button-text"
+        autoFocus
+      />
+      <Button
+        label="Save"
+        icon="icon icon-common icon-database-submit"
+        onClick={() => {
+          editOrgs();
+          setDisplayEditContainer(false);
+        }}
+      />
+    </div>
+  );
 
   let generateApporgsDivs = appVars.appOrgs.map((appOrg) => {
     if (appOrg.alias === primary) {
@@ -52,10 +92,34 @@ const OrgChart = ({ activeOrgs, primary }) => {
   return (
     <div>
       <div class="card">
-        <div class="flex flex-row flex-wrap card-container blue-container">
+        <ContextMenu model={contextMenuItems} ref={cm}></ContextMenu>
+        <div
+          class="flex flex-row flex-wrap card-container"
+          onContextMenu={(e) => cm.current.show(e)}
+        >
           {generateApporgsDivs}
         </div>
       </div>
+
+      <Dialog
+        header={headerEditDialog}
+        visible={displayEditContainer}
+        closable={false}
+        draggable={true}
+        style={{ width: "50vw" }}
+        onHide={() => setDisplayEditContainer(false)}
+        footer={footerEditDialog}
+      >
+        <div className="flex flex-wrap card-container">
+          <SelectButton
+            value={edittedOrgs}
+            options={appVars.appOrgs}
+            onChange={(e) => setEdittedOrgs(e.value)}
+            optionLabel="alias"
+            multiple
+          />
+        </div>
+      </Dialog>
     </div>
   );
 };
