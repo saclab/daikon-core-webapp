a7b0ad8b76b115460b24223c85bcb35826893b7c
Siddhant Rath <sid@tamu.edu>
2023-01-31 21:11:18 -0600

    orgs added

--- src/scene-d/Portfolio/PortfolioView/PortfolioInformation/LocalComponents/PortfolioInformationGeneralInformation.js
+++ src/scene-d/Portfolio/PortfolioView/PortfolioInformation/LocalComponents/PortfolioInformationGeneralInformation.js
@@ -1,56 +1,85 @@
+import { Column } from "primereact/column";
+import { DataTable } from "primereact/datatable";
 import React from "react";
-import ProjectStatus from "../../../../../app/common/ProjectStatus/ProjectStatus";
 import StageTag from "../../../../../app/common/StageTag/StageTag";
+import TagGeneral from "../../../../../app/common/TagGeneral/TagGeneral";
 import "./LocalComponents.css";
 
 const PortfolioInformationGeneralInformation = ({ project }) => {
   let displaySupportingOrgs =
     project.supportingOrgs.length !== 0
       ? project.supportingOrgs.map((org) => {
-        return <p>{org.appOrg.name}</p>;
-      })
+          return <p>{org.appOrg.name}</p>;
+        })
       : null;
 
+  let data = [
+    {
+      name: "Id",
+      value: project.id,
+    },
+    {
+      name: "Target",
+      value: (
+        <div>
+          <i className="icon icon-common icon-target" />{" "}
+          {project.targetName ? project.targetName : "Unknown"}
+        </div>
+      ),
+    },
+    {
+      name: "Current Stage",
+      value: <StageTag stage={project.currentStage} />,
+    },
+    {
+      name: "Project Status",
+      value: <TagGeneral tag={project.status} />,
+    },
+    {
+      name: "H2L Description",
+      value: (
+        <div
+          className="overflow-hidden text-overflow-ellipsis"
+          style={{ maxWidth: "400px" }}
+        >
+          {project.h2LDescription}
+        </div>
+      ),
+    },
+  ];
+  if (project.loEnabled) {
+    data.push({
+      name: "LO Description",
+      value: (
+        <div
+          className="overflow-hidden text-overflow-ellipsis"
+          style={{ maxWidth: "400px" }}
+        >
+          {project.loDescription}
+        </div>
+      ),
+    });
+  }
+  if (project.spEnabled) {
+    data.push({
+      name: "SP Description",
+      value: (
+        <div
+          className="overflow-hidden text-overflow-ellipsis"
+          style={{ maxWidth: "400px" }}
+        >
+          {project.spDescription}
+        </div>
+      ),
+    });
+  }
+
   return (
-    <div className="flex flex-column w-full">
-      <div className="flex flex-column">
-        <ProjectStatus status={project.status} />
-      </div>
-      <div className="flex align-content-center">
-        <p>Expanded Id : {project.id}</p>
-      </div>
-      <div className="flex flex-column mb-2" style={{ width: "30rem", lineHeight: "100%" }}>
-        <div className="flex">{project.h2LDescription ? '(H2L) ' + project.h2LDescription : ''}</div>
-        <div className="flex">{project.loDescription ? '(LO) ' + project.loDescription : ''}</div>
-        <div className="flex">{project.spDescription ? '(SP) ' + project.spDescription : ''}</div>
-      </div>
-
-      <div className="flex flex-column">
-        <table>
-          <tr>
-            <td>Target:</td>
-            <td><b>{project.targetName}</b></td>
-          </tr>
-          <tr>
-            <td>Current Stage :</td>
-            <td><b> <StageTag stage={project.currentStage} /></b></td>
-          </tr>
-          <tr>
-            <td>Status:</td>
-            <td><b>{project.status}</b></td>
-          </tr>
-          <tr>
-            <td>Primary Org:</td>
-            <td><b>{project?.primaryOrg?.name}</b></td>
-          </tr>
-          <tr>
-            <td>Supporting Orgs:</td>
-            <td><b>{displaySupportingOrgs}</b></td>
-          </tr>
-        </table>
-
-      </div>
-
+    <div className="flex flex-column flex-wrap card-container min-w-max">
+      <DataTable className="noDataTableHeader" value={data}>
+        <Column field="name"></Column>
+        <Column field="value"></Column>
+      </DataTable>
     </div>
   );
 };
