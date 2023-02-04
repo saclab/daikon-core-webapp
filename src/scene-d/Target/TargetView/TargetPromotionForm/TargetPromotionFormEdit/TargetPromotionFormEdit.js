e86b73b750f37cfa04f547a3e07197b48cec3c75
Siddhant Rath <sid@tamu.edu>
2023-02-03 21:26:42 -0600

    Cleanup

--- src/scene-d/Target/TargetView/TargetPromotionForm/TargetPromotionFormEdit/TargetPromotionFormEdit.js
+++ src/scene-d/Target/TargetView/TargetPromotionForm/TargetPromotionFormEdit/TargetPromotionFormEdit.js
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import Question from "../../../../../app/common/Question/Question";
 import SectionHeading from "../../../../../app/common/SectionHeading/SectionHeading";
+import { TargetPromotionInfoUserSection } from "../../../../../app/common/Values/Values";
 import Loading from "../../../../../app/layout/Loading/Loading";
 import { RootStoreContext } from "../../../../../app/stores/rootStore";
 import { appColors } from "../../../../../colors";
@@ -97,97 +98,7 @@ const TargetPromotionFormEdit = ({ data, selectedTarget }) => {
       //console.log(targetPromotionFormValue);
     };
 
-    let sections = [
-      {
-        heading: "Impact of Chemical Inhibition",
-        subSections: [
-          {
-            topic: "a) During infections",
-            questions: ["2a1", "2a1b", "2a2", "2a3a", "2a3b", "2a4a", "2a5"],
-          },
-          {
-            topic: "b) on replication Mtb in vitro",
-            questions: ["2b1", "2b2", "2b4"],
-          },
-          {
-            topic: "c) on nonreplicating Mtb in vitro",
-            questions: ["2c1", "2c2", "2c3", "2c4", "2c5"],
-          },
-        ],
-      },
-
-      {
-        heading: "Chemical Inhibition",
-        subSections: [
-          {
-            topic: "a) in live Mtb",
-            questions: ["3a1", "3a2", "3a3", "3a4"],
-          },
-          {
-            topic: "b) in vitro",
-            questions: ["3b1", "3b2"],
-          },
-        ],
-      },
-
-      {
-        heading: "Impact Of Genetic Inhibition",
-        subSections: [
-          {
-            topic: "a) During infections",
-            questions: ["4a1", "4a2a", "4a2b", "4a3a", "4a3b", "4a4"],
-          },
-          {
-            topic: "b) on replication Mtb in vitro",
-            questions: ["4b1", "4b2", "4b3"],
-          },
-          {
-            topic: "c) on nonreplicating Mtb in vitro",
-            questions: ["4c1", "4c2", "4c3", "4c4", "4c5"],
-          },
-        ],
-      },
-
-      {
-        heading: "Liabilities",
-        subSections: [
-          {
-            topic: "Metabolic liabilities",
-            questions: ["5a1", "5a2"],
-          },
-          {
-            topic: "Genetic",
-            questions: ["5a3"],
-          },
-          {
-            topic: "Other",
-            questions: ["5b1"],
-          },
-        ],
-      },
-
-      {
-        heading: "Tractability",
-        subSections: [
-          {
-            topic: "a) High throughput screening feasibility",
-            questions: ["6a1", "6a2", "6a3", "6a4", "6a5", "6a6", "6a7"],
-          },
-          {
-            topic: "b) Structure based feasibility",
-            questions: ["6b1", "6b2", "6b3", "6b4", "6b5"],
-          },
-          {
-            topic: "c) Progressibility considerations",
-            questions: ["6c1", "6c2", "6c3", "6c4", "6c5", "6c6"],
-          },
-          {
-            topic: "d) Safety considerations",
-            questions: ["6d1", "6d2", "6d3", "6d4"],
-          },
-        ],
-      },
-    ];
+    let sections = TargetPromotionInfoUserSection;
     let generateForm = () => {
       return sections.map((section) => {
         let generateSubsections = section.subSections.map((subSection) => {
