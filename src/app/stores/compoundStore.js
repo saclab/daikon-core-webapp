f6f6a869f57390da617cfdf58c1984afb4b308d1
Siddhant Rath <sid@tamu.edu>
2023-03-07 16:01:16 -0600

    Compound Dash

--- src/app/stores/compoundStore.js
+++ src/app/stores/compoundStore.js
@@ -14,9 +14,12 @@ export default class CompoundStore {
 
   displayLoading = false;
   compoundRegistry = new Map();
+  compoundRegistryExpanded = new Map();
   cacheValid = false;
 
   selectedCompound = null;
+
+  loadingFetchCompound = false;
 
   constructor(rootStore) {
     this.rootStore = rootStore;
@@ -24,20 +27,32 @@ export default class CompoundStore {
       displayLoading: observable,
       fetchCompounds: action,
       compounds: computed,
+      compoundRegistry: observable,
 
       selectedCompound: observable,
+
+      loadingFetchCompound: observable,
+      fetchCompound: action,
+      compoundRegistryExpanded: observable,
     });
   }
 
   fetchCompounds = async () => {
     this.displayLoading = true;
+
+    if (this.cacheValid) {
+      this.displayLoading = false;
+      return;
+    }
+
     try {
-      const compounds = await agent.Compounds.list();
+      let fetchedCompounds = await agent.Compounds.list();
       runInAction(() => {
-        compounds.forEach((compound) => {
+        fetchedCompounds.forEach((compound) => {
           this.compoundRegistry.set(compound.id, compound);
         });
         this.displayLoading = false;
+        this.cacheValid = true;
       });
     } catch (error) {
       runInAction(() => {
@@ -51,4 +66,31 @@ export default class CompoundStore {
   get compounds() {
     return Array.from(this.compoundRegistry.values());
   }
+
+  fetchCompound = async (id, invalidateCache = false) => {
+    this.loadingFetchCompound = true;
+
+    // first check cache
+    let fetchedCompound = this.compoundRegistryExpanded.get(id);
+    if (!invalidateCache && fetchedCompound) {
+      this.selectedCompound = fetchedCompound;
+      this.loadingFetchCompound = false;
+    }
+    // if not found fetch from api
+    else {
+      try {
+        fetchedCompound = await agent.Compounds.details(id);
+        runInAction(() => {
+          this.selectedCompound = fetchedCompound;
+          this.compoundRegistryExpanded.set(id, fetchedCompound);
+        });
+      } catch (error) {
+        console.error(error);
+      } finally {
+        runInAction(() => {
+          this.loadingFetchCompound = false;
+        });
+      }
+    }
+  };
 }
