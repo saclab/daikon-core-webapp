9fac3592e0cd4dbfdfc6eda92aac2dcb14cabb74
Siddhant Rath <sid@tamu.edu>
2023-03-08 13:07:36 -0600

    Compound Overview

--- src/screen-tools/Compounds/CompoundView/CompoundOverview/CompoundOverview.js
+++ src/screen-tools/Compounds/CompoundView/CompoundOverview/CompoundOverview.js
@@ -1,13 +1,21 @@
+import _ from "lodash";
 import { BreadCrumb } from "primereact/breadcrumb";
 import { Fieldset } from "primereact/fieldset";
-import React from "react";
-import { NavLink, useNavigate } from "react-router-dom";
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import SectionHeading from "../../../../app/common/SectionHeading/SectionHeading";
 import SmilesView from "../../../../app/common/SmilesView/SmilesView";
+import { RootStoreContext } from "../../../../app/stores/rootStore";
+import { appColors } from "../../../../colors";
+import CompoundOverviewEditExternalId from "./LocalComponents/CompoundOverviewEditExternalId";
+import CompoundOverviewEditProperties from "./LocalComponents/CompoundOverviewEditProperties";
 import CompoundOverviewGeneralInformation from "./LocalComponents/CompoundOverviewGeneralInformation";
 
 const CompoundOverview = ({ selectedCompound }) => {
   const navigate = useNavigate();
+
+  const rootStore = useContext(RootStoreContext);
+  const { user } = rootStore.userStore;
 
   const breadCrumbItems = [
     { label: "Tools" },
@@ -30,26 +38,108 @@ const CompoundOverview = ({ selectedCompound }) => {
     selectedCompound.projectsWithPrimaryStructure.map((project) => {
       return (
         <li key={project.id + "pri"}>
-          <NavLink to={"/d/portfolio/" + project.id}>
-            {project.projectName}
-          </NavLink>
+          <div className="flex flex-row gap-2">{project.projectName}</div>
         </li>
       );
     });
 
   let asProjectsWithBaseHits = selectedCompound.projectsWithBaseHits.map(
     (project) => {
-      return <li key={project.id + "base"}>{project.projectName}</li>;
+      return (
+        <li key={project.id + "base"}>
+          <div className="flex flex-row gap-2">{project.projectName}</div>
+        </li>
+      );
+    }
+  );
+
+  let inCEofProjects = selectedCompound.projectsWithCompoundEvolution.map(
+    (project) => {
+      return (
+        <li key={project.id + "pri"}>
+          <div className="flex flex-row gap-2">{project.projectName}</div>
+        </li>
+      );
     }
   );
 
   let asHitsInScreen = selectedCompound.screen.map((screen) => {
     return (
-      <li key={screen.id + "screen"}>
-        {screen.screenName}, {screen.screenType}
+      <li key={screen.id + "pri"}>
+        <div className="flex flex-row gap-2">
+          {screen.screenName} [{_.startCase(screen.screenType)}]
+        </div>
       </li>
     );
   });
+
+  /* S E C T IO N S */
+  const basicInformation = (
+    <React.Fragment>
+      <div className="flex w-full">
+        <div className="flex">
+          <Fieldset legend="Details">
+            <CompoundOverviewGeneralInformation
+              selectedCompound={selectedCompound}
+            />
+          </Fieldset>
+          <div className="flex">
+            <Fieldset legend="Structure">
+              <SmilesView
+                smiles={selectedCompound.smiles}
+                width="600"
+                height="350"
+              />
+            </Fieldset>
+          </div>
+        </div>
+      </div>
+      <div className="flex w-full">
+        <div className="flex">
+          <Fieldset legend="As primary structure of projects">
+            <ul>{asPrimaryStructureOfProjects}</ul>
+          </Fieldset>
+        </div>
+
+        <div className="flex">
+          <Fieldset legend="As base hits of projects">
+            <ul>{asProjectsWithBaseHits}</ul>
+          </Fieldset>
+        </div>
+
+        <div className="flex">
+          <Fieldset legend="In compound evolution of projects">
+            <ul>{inCEofProjects}</ul>
+          </Fieldset>
+        </div>
+
+        <div className="flex">
+          <Fieldset legend="As hits in screens">
+            <ul>{asHitsInScreen}</ul>
+          </Fieldset>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+  const editProperties = (
+    <React.Fragment>
+      <div className="flex">
+        <Fieldset legend="Edit">
+          <CompoundOverviewEditProperties selectedCompound={selectedCompound} />
+        </Fieldset>
+      </div>
+    </React.Fragment>
+  );
+
+  const editExternalIdDisplay = (
+    <React.Fragment>
+      <div className="flex">
+        <Fieldset legend="Edit External Id">
+          <CompoundOverviewEditExternalId selectedCompound={selectedCompound} />
+        </Fieldset>
+      </div>
+    </React.Fragment>
+  );
 
   return (
     <React.Fragment>
@@ -57,51 +147,18 @@ const CompoundOverview = ({ selectedCompound }) => {
         <div className="flex w-full pb-2">
           <BreadCrumb model={breadCrumbItems} />
         </div>
-
         <div className="flex w-full">
           <SectionHeading
             icon="icon icon-common icon-math"
             heading={selectedCompound.externalCompoundIds}
             displayHorizon={false}
-            // color={appColors.sectionHeadingBg.portfolio}
+            color={appColors.sectionHeadingBg.compounds}
           />
         </div>
+        {basicInformation}
         <div className="flex w-full">
-          <div className="flex">
-            <Fieldset legend="Details">
-              <CompoundOverviewGeneralInformation
-                selectedCompound={selectedCompound}
-              />
-            </Fieldset>
-            <div className="flex">
-              <Fieldset legend="Structure">
-                <SmilesView
-                  smiles={selectedCompound.smiles}
-                  width="600"
-                  height="350"
-                />
-              </Fieldset>
-            </div>
-          </div>
-        </div>
-        <div className="flex w-full">
-          <div className="flex">
-            <Fieldset legend="As primary structure of projects">
-              <ul>{asPrimaryStructureOfProjects}</ul>
-            </Fieldset>
-          </div>
-
-          <div className="flex">
-            <Fieldset legend="As base hits of projects">
-              <ul>{asProjectsWithBaseHits}</ul>
-            </Fieldset>
-          </div>
-
-          <div className="flex">
-            <Fieldset legend="As hits in screens">
-              <ul>{asHitsInScreen}</ul>
-            </Fieldset>
-          </div>
+          {editProperties}
+          {user.roles.includes("projectManager") ? editExternalIdDisplay : ""}
         </div>
       </div>
     </React.Fragment>
